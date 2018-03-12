Asunny-ren/cardano-sl

[CSL-2165] Add timestamp to output of `GET api/v1/transactions` endpoint
First, `Transaction` type now contains timestamp field. Time is represented
as fractional number of seconds, with precision up to microseconds.
Example: `1519926196.536234`

Second, this allowed to enable filtering and sorting by time via
general existing mechanisms.
E.g. one can pass `?timestamp=LTE[1519926196.536234]` in request
and see only transactions made after mentioned point in time.

wallet-new/src/Cardano/Wallet/API/Indices.hs
@@ -6,11 +6,13 @@
 {-# OPTIONS_GHC -fno-warn-orphans      #-}
 module Cardano.Wallet.API.Indices where
 
+import qualified Control.Lens as Lens
 import           Universum
 
 import           Cardano.Wallet.API.V1.Types
 import           Data.String.Conv (toS)
 import qualified Pos.Core as Core
+import           Pos.Crypto (decodeHash)
 
 import           Data.IxSet.Typed (Indexable (..), IsIndexOf, IxSet, ixFun, ixList)
 
@@ -33,6 +35,15 @@ instance ToIndex Wallet Core.Coin where
         Just c  -> Just (Core.mkCoin c)
     accessIx Wallet{..} = let (V1 balance) = walBalance in balance
 
+instance ToIndex Transaction Core.TxId where
+    toIndex _ = rightToMaybe . decodeHash
+    accessIx Transaction{..} = let V1 ti = txId in ti
+
+instance ToIndex Transaction Core.Timestamp where
+    toIndex _ x =
+        view (Lens.from Core.timestampSeconds) <$> readMaybe @Double (toS x)
+    accessIx Transaction{..} = let V1 time = txTimestamp in time
+
 -- | A type family mapping a resource 'a' to all its indices.
 type family IndicesOf a :: [*] where
     IndicesOf Wallet      = WalletIxs
@@ -54,7 +65,7 @@ type IsIndexOf' a ix = IsIndexOf ix (IndicesOf a)
 
 -- | The indices for each major resource.
 type WalletIxs      = '[WalletId, Core.Coin]
-type TransactionIxs = '[Core.TxId]
+type TransactionIxs = '[Core.TxId, Core.Timestamp]
 type AccountIxs     = '[AccountIndex]
 
 instance Indexable WalletIxs Wallet where
@@ -63,6 +74,7 @@ instance Indexable WalletIxs Wallet where
 
 instance Indexable TransactionIxs Transaction where
   indices = ixList (ixFun (\Transaction{..} -> let (V1 tid) = txId in [tid]))
+                   (ixFun (\Transaction{..} -> let (V1 time) = txTimestamp in [time]))
 
 instance Indexable AccountIxs Account where
   indices = ixList (ixFun (\Account{..} -> [accIndex]))

wallet-new/src/Cardano/Wallet/API/Request/Filter.hs
@@ -82,6 +82,7 @@ data FilterBy (sym :: [Symbol]) (r :: *) deriving Typeable
 -- indices, so that we can later on reify them into a list of valid indices.
 type family FilterParams (syms :: [Symbol]) (r :: *) :: [*] where
     FilterParams '["wallet_id", "balance"] Wallet = IndicesOf Wallet
+    FilterParams '["tx_id", "timestamp"] Transaction = IndicesOf Transaction
 
 class ToFilterOperations (ixs :: [*]) a where
   toFilterOperations :: Request -> [Text] -> proxy ixs -> FilterOperations a

wallet-new/src/Cardano/Wallet/API/Request/Sort.hs
@@ -74,6 +74,7 @@ flattenSortOperations (SortOp f fs) = show f : flattenSortOperations fs
 -- much sense, so we restrict ourselves.
 type family SortParams (syms :: [Symbol]) (r :: *) :: [*] where
     SortParams '["balance"] Wallet = '[Core.Coin]
+    SortParams '["timestamp"] Transaction = '[Core.Timestamp]
 
 -- | Handy typeclass to reconcile type and value levels by building a list of 'SortOperation' out of
 -- a type level list.

input-output-hk/cardano-wallet-legacy

src/Cardano/Wallet/API/Indices.hs
@@ -6,11 +6,13 @@
 {-# OPTIONS_GHC -fno-warn-orphans      #-}
 module Cardano.Wallet.API.Indices where
 
+import qualified Control.Lens as Lens
 import           Universum
 
 import           Cardano.Wallet.API.V1.Types
 import           Data.String.Conv (toS)
 import qualified Pos.Core as Core
+import           Pos.Crypto (decodeHash)
 
 import           Data.IxSet.Typed (Indexable (..), IsIndexOf, IxSet, ixFun, ixList)
 
@@ -33,6 +35,15 @@ instance ToIndex Wallet Core.Coin where
         Just c  -> Just (Core.mkCoin c)
     accessIx Wallet{..} = let (V1 balance) = walBalance in balance
 
+instance ToIndex Transaction Core.TxId where
+    toIndex _ = rightToMaybe . decodeHash
+    accessIx Transaction{..} = let V1 ti = txId in ti
+
+instance ToIndex Transaction Core.Timestamp where
+    toIndex _ x =
+        view (Lens.from Core.timestampSeconds) <$> readMaybe @Double (toS x)
+    accessIx Transaction{..} = let V1 time = txCreationTime in time
+
 -- | A type family mapping a resource 'a' to all its indices.
 type family IndicesOf a :: [*] where
     IndicesOf Wallet      = WalletIxs
@@ -54,7 +65,7 @@ type IsIndexOf' a ix = IsIndexOf ix (IndicesOf a)
 
 -- | The indices for each major resource.
 type WalletIxs      = '[WalletId, Core.Coin]
-type TransactionIxs = '[Core.TxId]
+type TransactionIxs = '[Core.TxId, Core.Timestamp]
 type AccountIxs     = '[AccountIndex]
 
 instance Indexable WalletIxs Wallet where
@@ -63,6 +74,7 @@ instance Indexable WalletIxs Wallet where
 
 instance Indexable TransactionIxs Transaction where
   indices = ixList (ixFun (\Transaction{..} -> let (V1 tid) = txId in [tid]))
+                   (ixFun (\Transaction{..} -> let (V1 time) = txCreationTime in [time]))
 
 instance Indexable AccountIxs Account where
   indices = ixList (ixFun (\Account{..} -> [accIndex]))

src/Cardano/Wallet/API/Request/Filter.hs
@@ -82,6 +82,7 @@ data FilterBy (sym :: [Symbol]) (r :: *) deriving Typeable
 -- indices, so that we can later on reify them into a list of valid indices.
 type family FilterParams (syms :: [Symbol]) (r :: *) :: [*] where
     FilterParams '["wallet_id", "balance"] Wallet = IndicesOf Wallet
+    FilterParams '["id", "created_at"] Transaction = IndicesOf Transaction
 
 class ToFilterOperations (ixs :: [*]) a where
   toFilterOperations :: Request -> [Text] -> proxy ixs -> FilterOperations a

src/Cardano/Wallet/API/Request/Sort.hs
@@ -74,6 +74,7 @@ flattenSortOperations (SortOp f fs) = show f : flattenSortOperations fs
 -- much sense, so we restrict ourselves.
 type family SortParams (syms :: [Symbol]) (r :: *) :: [*] where
     SortParams '["balance"] Wallet = '[Core.Coin]
+    SortParams '["id"] Transaction = '[Core.Timestamp]
 
 -- | Handy typeclass to reconcile type and value levels by building a list of 'SortOperation' out of
 -- a type level list.