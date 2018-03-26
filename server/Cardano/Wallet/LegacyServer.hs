testtkxxd/cardano-sl

[CSL-2346] Updated cardano-sl-wallet-new

wallet-new/server/Cardano/Wallet/LegacyServer.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 module Cardano.Wallet.LegacyServer where
 
+import           Universum (TVar)
+
 import           Cardano.Wallet.API
 import           Cardano.Wallet.API.V1.Migration as Migration
 
@@ -9,6 +11,7 @@ import qualified Cardano.Wallet.API.Development.LegacyHandlers as Dev
 import qualified Cardano.Wallet.API.V1.LegacyHandlers as V1
 import           Cardano.Wallet.Server.CLI (RunMode (..))
 
+import           Ntp.Client (NtpStatus)
 import           Pos.Diffusion.Types (Diffusion (..))
 import           Pos.Wallet.Web.Mode (WalletWebMode)
 import           Servant
@@ -21,10 +24,11 @@ walletServer :: ( Migration.HasConfigurations
                 )
              => (forall a. WalletWebMode a -> Handler a)
              -> Diffusion WalletWebMode
+             -> TVar NtpStatus
              -> RunMode
              -> Server WalletAPI
-walletServer natV0 diffusion runMode = externalAPI :<|> internalAPI
+walletServer natV0 diffusion ntpStatus runMode = externalAPI :<|> internalAPI
   where
-    externalAPI =  V0.handlers natV0 diffusion
-              :<|> V1.handlers natV0 diffusion
+    externalAPI =  V0.handlers natV0 diffusion ntpStatus
+              :<|> V1.handlers natV0 diffusion ntpStatus
     internalAPI =  Dev.handlers natV0 runMode

input-output-hk/cardano-wallet-legacy

server/Cardano/Wallet/LegacyServer.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 module Cardano.Wallet.LegacyServer where
 
+import           Universum (TVar)
+
 import           Cardano.Wallet.API
 import           Cardano.Wallet.API.V1.Migration as Migration
 
@@ -10,6 +12,7 @@ import qualified Cardano.Wallet.API.V1.LegacyHandlers as V1
 import qualified Cardano.Wallet.API.V1.Swagger as Swagger
 import           Cardano.Wallet.Server.CLI (RunMode (..))
 
+import           Ntp.Client (NtpStatus)
 import           Pos.Diffusion.Types (Diffusion (..))
 import           Pos.Update.Configuration (curSoftwareVersion)
 import           Pos.Util.CompileInfo (compileInfo)
@@ -23,22 +26,24 @@ import           Servant.Swagger.UI (swaggerSchemaUIServer)
 walletServer :: (Migration.HasConfigurations, Migration.HasCompileInfo)
              => (forall a. WalletWebMode a -> Handler a)
              -> Diffusion WalletWebMode
+             -> TVar NtpStatus
              -> Server WalletAPI
-walletServer natV0 diffusion = v0DocHandler :<|> v1DocHandler :<|> v0Handler :<|> v1Handler
+walletServer natV0 diffusion ntpStatus = v0DocHandler :<|> v1DocHandler :<|> v0Handler :<|> v1Handler
   where
     v0DocHandler = swaggerSchemaUIServer (Swagger.api (compileInfo, curSoftwareVersion) v0API Swagger.highLevelShortDescription)
-    v0Handler    = V0.handlers natV0 diffusion
+    v0Handler    = V0.handlers natV0 diffusion ntpStatus
     v1DocHandler = swaggerSchemaUIServer (Swagger.api (compileInfo, curSoftwareVersion) v1API Swagger.highLevelDescription)
-    v1Handler    = V1.handlers natV0 diffusion
+    v1Handler    = V1.handlers natV0 diffusion ntpStatus
 
 
 walletDevServer :: (Migration.HasConfigurations, Migration.HasCompileInfo)
              => (forall a. WalletWebMode a -> Handler a)
              -> Diffusion WalletWebMode
+             -> TVar NtpStatus
              -> RunMode
              -> Server WalletDevAPI
-walletDevServer natV0 diffusion runMode = devDocHandler :<|> devHandler :<|> walletHandler
+walletDevServer natV0 diffusion ntpStatus runMode = devDocHandler :<|> devHandler :<|> walletHandler
   where
     devDocHandler = swaggerSchemaUIServer (Swagger.api (compileInfo, curSoftwareVersion) devAPI Swagger.highLevelShortDescription)
     devHandler    = Dev.handlers natV0 runMode
-    walletHandler = walletServer natV0 diffusion
+    walletHandler = walletServer natV0 diffusion ntpStatus