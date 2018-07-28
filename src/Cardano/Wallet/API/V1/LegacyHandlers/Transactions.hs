hadasKatz/cardano-sl

Removed HasTxpConfiguration

wallet-new/src/Cardano/Wallet/API/V1/LegacyHandlers/Transactions.hs
@@ -11,6 +11,7 @@ import qualified Pos.Client.Txp.Util as V0
 import qualified Pos.Core as Core
 import           Pos.Core.Txp (TxAux)
 import           Pos.Crypto (ProtocolMagic)
+import           Pos.Txp.Configuration (TxpConfiguration)
 import qualified Pos.Util.Servant as V0
 import qualified Pos.Wallet.WalletMode as V0
 import qualified Pos.Wallet.Web.ClientTypes.Types as V0
@@ -32,10 +33,11 @@ import           Cardano.Wallet.API.V1.Types
 handlers
     :: HasConfigurations
     => ProtocolMagic
+    -> TxpConfiguration
     -> (TxAux -> MonadV1 Bool)
     -> ServerT Transactions.API MonadV1
-handlers pm submitTx =
-             newTransaction pm submitTx
+handlers pm txpConfig submitTx =
+             newTransaction pm txpConfig submitTx
         :<|> allTransactions
         :<|> estimateFees pm
 
@@ -43,10 +45,11 @@ newTransaction
     :: forall ctx m
      . (V0.MonadWalletTxFull ctx m)
     => ProtocolMagic
+    -> TxpConfiguration
     -> (TxAux -> m Bool)
     -> Payment
     -> m (WalletResponse Transaction)
-newTransaction pm submitTx Payment {..} = do
+newTransaction pm txpConfig submitTx Payment {..} = do
     ws <- V0.askWalletSnapshot
     sourceWallet <- migrate (psWalletId pmtSource)
 
@@ -69,7 +72,7 @@ newTransaction pm submitTx Payment {..} = do
     addrCoinList <- migrate $ NE.toList pmtDestinations
     let (V1 policy) = fromMaybe (V1 defaultInputSelectionPolicy) pmtGroupingPolicy
     let batchPayment = V0.NewBatchPayment cAccountId addrCoinList policy
-    cTx <- V0.newPaymentBatch pm submitTx spendingPw batchPayment
+    cTx <- V0.newPaymentBatch pm txpConfig submitTx spendingPw batchPayment
     single <$> migrate cTx
 
 

wallet-new/src/Cardano/Wallet/LegacyServer.hs
@@ -13,6 +13,7 @@ import           Cardano.Wallet.Server.CLI (RunMode (..))
 import           Ntp.Client (NtpStatus)
 import           Pos.Crypto (ProtocolMagic)
 import           Pos.Infra.Diffusion.Types (Diffusion (..))
+import           Pos.Txp.Configuration (TxpConfiguration)
 import           Pos.Update.Configuration (curSoftwareVersion)
 import           Pos.Util.CompileInfo (compileInfo)
 import           Pos.Wallet.Web.Mode (WalletWebMode)
@@ -29,18 +30,19 @@ import qualified Cardano.Wallet.API.V1.Swagger as Swagger
 walletServer :: (HasConfigurations, HasCompileInfo)
              => (forall a. WalletWebMode a -> Handler a)
              -> ProtocolMagic
+             -> TxpConfiguration
              -> Diffusion WalletWebMode
              -> TVar NtpStatus
              -> RunMode
              -> Server WalletAPI
-walletServer natV0 pm diffusion ntpStatus runMode =
+walletServer natV0 pm txpConfig diffusion ntpStatus runMode =
          v0Handler
     :<|> v0Handler
     :<|> v1Handler
     :<|> internalHandler
   where
-    v0Handler       = V0.handlers natV0 pm diffusion ntpStatus
-    v1Handler       = V1.handlers natV0 pm diffusion ntpStatus
+    v0Handler       = V0.handlers natV0 pm txpConfig diffusion ntpStatus
+    v1Handler       = V1.handlers natV0 pm txpConfig diffusion ntpStatus
     internalHandler = Internal.handlers natV0 runMode
 
 

wallet-new/src/Cardano/Wallet/Server/Plugins.hs
@@ -45,6 +45,7 @@ import           Network.Wai.Middleware.Cors (cors, corsMethods,
                      simpleMethods)
 import           Ntp.Client (NtpStatus)
 import           Pos.Infra.Diffusion.Types (Diffusion (..))
+import           Pos.Txp.Configuration (TxpConfiguration)
 import           Pos.Wallet.Web (cleanupAcidStatePeriodically)
 import           Pos.Wallet.Web.Pending.Worker (startPendingTxsResubmitter)
 import qualified Pos.Wallet.Web.Server.Runner as V0
@@ -116,10 +117,11 @@ walletDocumentation WalletBackendParams {..} = pure $ \_ ->
 -- | A @Plugin@ to start the wallet backend API.
 legacyWalletBackend :: (HasConfigurations, HasCompileInfo)
                     => ProtocolMagic
+                    -> TxpConfiguration
                     -> WalletBackendParams
                     -> TVar NtpStatus
                     -> Plugin WalletWebMode
-legacyWalletBackend pm WalletBackendParams {..} ntpStatus = pure $ \diffusion -> do
+legacyWalletBackend pm txpConfig WalletBackendParams {..} ntpStatus = pure $ \diffusion -> do
     modifyLoggerName (const "legacyServantBackend") $ do
       logInfo $ sformat ("Production mode for API: "%build)
         walletProductionApi
@@ -151,6 +153,7 @@ legacyWalletBackend pm WalletBackendParams {..} ntpStatus = pure $ \diffusion ->
             $ LegacyServer.walletServer
                 (V0.convertHandler ctx)
                 pm
+                txpConfig
                 diffusion
                 ntpStatus
                 walletRunMode
@@ -223,9 +226,12 @@ walletBackend protocolMagic (NewWalletBackendParams WalletBackendParams{..}) (pa
     lower env m = runReaderT m env
 
 -- | A @Plugin@ to resubmit pending transactions.
-resubmitterPlugin :: HasConfigurations => ProtocolMagic -> Plugin WalletWebMode
-resubmitterPlugin pm = [\diffusion -> askWalletDB >>= \db ->
-                        startPendingTxsResubmitter pm db (sendTx diffusion)]
+resubmitterPlugin :: HasConfigurations
+                  => ProtocolMagic
+                  -> TxpConfiguration
+                  -> Plugin WalletWebMode
+resubmitterPlugin pm txpConfig = [\diffusion -> askWalletDB >>= \db ->
+                        startPendingTxsResubmitter pm txpConfig db (sendTx diffusion)]
 
 -- | A @Plugin@ to notify frontend via websockets.
 notifierPlugin :: HasConfigurations => Plugin WalletWebMode

input-output-hk/cardano-wallet-legacy

src/Cardano/Wallet/API/V1/LegacyHandlers/Transactions.hs
@@ -6,6 +6,7 @@ import qualified Data.IxSet.Typed as IxSet
 import qualified Data.List.NonEmpty as NE
 import           Servant
 
+import           Pos.Chain.Txp (TxpConfiguration)
 import           Pos.Client.Txp.Util (defaultInputSelectionPolicy)
 import qualified Pos.Client.Txp.Util as V0
 import qualified Pos.Core as Core
@@ -32,10 +33,11 @@ import           Cardano.Wallet.API.V1.Types
 handlers
     :: HasConfigurations
     => ProtocolMagic
+    -> TxpConfiguration
     -> (TxAux -> MonadV1 Bool)
     -> ServerT Transactions.API MonadV1
-handlers pm submitTx =
-             newTransaction pm submitTx
+handlers pm txpConfig submitTx =
+             newTransaction pm txpConfig submitTx
         :<|> allTransactions
         :<|> estimateFees pm
 
@@ -43,10 +45,11 @@ newTransaction
     :: forall ctx m
      . (V0.MonadWalletTxFull ctx m)
     => ProtocolMagic
+    -> TxpConfiguration
     -> (TxAux -> m Bool)
     -> Payment
     -> m (WalletResponse Transaction)
-newTransaction pm submitTx Payment {..} = do
+newTransaction pm txpConfig submitTx Payment {..} = do
     ws <- V0.askWalletSnapshot
     sourceWallet <- migrate (psWalletId pmtSource)
 
@@ -69,7 +72,7 @@ newTransaction pm submitTx Payment {..} = do
     addrCoinList <- migrate $ NE.toList pmtDestinations
     let (V1 policy) = fromMaybe (V1 defaultInputSelectionPolicy) pmtGroupingPolicy
     let batchPayment = V0.NewBatchPayment cAccountId addrCoinList policy
-    cTx <- V0.newPaymentBatch pm submitTx spendingPw batchPayment
+    cTx <- V0.newPaymentBatch pm txpConfig submitTx spendingPw batchPayment
     single <$> migrate cTx
 
 

src/Cardano/Wallet/LegacyServer.hs
@@ -11,6 +11,7 @@ import           Cardano.Wallet.API.V1.Migration (HasCompileInfo,
 import           Cardano.Wallet.API.V1.Swagger (swaggerSchemaUIServer)
 import           Cardano.Wallet.Server.CLI (RunMode (..))
 import           Ntp.Client (NtpStatus)
+import           Pos.Chain.Txp (TxpConfiguration)
 import           Pos.Chain.Update (curSoftwareVersion)
 import           Pos.Crypto (ProtocolMagic)
 import           Pos.Infra.Diffusion.Types (Diffusion (..))
@@ -29,18 +30,19 @@ import qualified Cardano.Wallet.API.V1.Swagger as Swagger
 walletServer :: (HasConfigurations, HasCompileInfo)
              => (forall a. WalletWebMode a -> Handler a)
              -> ProtocolMagic
+             -> TxpConfiguration
              -> Diffusion WalletWebMode
              -> TVar NtpStatus
              -> RunMode
              -> Server WalletAPI
-walletServer natV0 pm diffusion ntpStatus runMode =
+walletServer natV0 pm txpConfig diffusion ntpStatus runMode =
          v0Handler
     :<|> v0Handler
     :<|> v1Handler
     :<|> internalHandler
   where
-    v0Handler       = V0.handlers natV0 pm diffusion ntpStatus
-    v1Handler       = V1.handlers natV0 pm diffusion ntpStatus
+    v0Handler       = V0.handlers natV0 pm txpConfig diffusion ntpStatus
+    v1Handler       = V1.handlers natV0 pm txpConfig diffusion ntpStatus
     internalHandler = Internal.handlers natV0 runMode
 
 

src/Cardano/Wallet/Server/Plugins.hs
@@ -44,6 +44,7 @@ import           Network.Wai.Middleware.Cors (cors, corsMethods,
                      corsRequestHeaders, simpleCorsResourcePolicy,
                      simpleMethods)
 import           Ntp.Client (NtpStatus)
+import           Pos.Chain.Txp (TxpConfiguration)
 import           Pos.Infra.Diffusion.Types (Diffusion (..))
 import           Pos.Wallet.Web (cleanupAcidStatePeriodically)
 import           Pos.Wallet.Web.Pending.Worker (startPendingTxsResubmitter)
@@ -116,10 +117,11 @@ walletDocumentation WalletBackendParams {..} = pure $ \_ ->
 -- | A @Plugin@ to start the wallet backend API.
 legacyWalletBackend :: (HasConfigurations, HasCompileInfo)
                     => ProtocolMagic
+                    -> TxpConfiguration
                     -> WalletBackendParams
                     -> TVar NtpStatus
                     -> Plugin WalletWebMode
-legacyWalletBackend pm WalletBackendParams {..} ntpStatus = pure $ \diffusion -> do
+legacyWalletBackend pm txpConfig WalletBackendParams {..} ntpStatus = pure $ \diffusion -> do
     modifyLoggerName (const "legacyServantBackend") $ do
       logInfo $ sformat ("Production mode for API: "%build)
         walletProductionApi
@@ -151,6 +153,7 @@ legacyWalletBackend pm WalletBackendParams {..} ntpStatus = pure $ \diffusion ->
             $ LegacyServer.walletServer
                 (V0.convertHandler ctx)
                 pm
+                txpConfig
                 diffusion
                 ntpStatus
                 walletRunMode
@@ -223,9 +226,12 @@ walletBackend protocolMagic (NewWalletBackendParams WalletBackendParams{..}) (pa
     lower env m = runReaderT m env
 
 -- | A @Plugin@ to resubmit pending transactions.
-resubmitterPlugin :: HasConfigurations => ProtocolMagic -> Plugin WalletWebMode
-resubmitterPlugin pm = [\diffusion -> askWalletDB >>= \db ->
-                        startPendingTxsResubmitter pm db (sendTx diffusion)]
+resubmitterPlugin :: HasConfigurations
+                  => ProtocolMagic
+                  -> TxpConfiguration
+                  -> Plugin WalletWebMode
+resubmitterPlugin pm txpConfig = [\diffusion -> askWalletDB >>= \db ->
+                        startPendingTxsResubmitter pm txpConfig db (sendTx diffusion)]
 
 -- | A @Plugin@ to notify frontend via websockets.
 notifierPlugin :: HasConfigurations => Plugin WalletWebMode