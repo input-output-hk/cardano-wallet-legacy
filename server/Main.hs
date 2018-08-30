input-output-hk/cardano-sl

[CBR-377] Separate Plugins into 'Plugins' and 'LegacyPlugins'
This will make it easier to remove the legacy stuff once we move
forward. Also, this commits adds the missing and (required) plugins to
the new wallet server. A few implementations are still missing (update
notifier and acid state snapshots) but will come.

Note that the new kernel registers itself to the node and receives new
block directly, there's no more need for a syncing pluging. Similarly,
the kernel takes care of managing restoration of wallets, even after a
node restart. Regarding the re-submission, this is currently done in
activeLayer bracket and the submissionLayer. This could be moved in a
later stage to a plugin for more consistency

wallet-new/server/Main.hs
@@ -49,6 +49,7 @@ import           Cardano.Wallet.Server.CLI (ChooseWalletBackend (..),
                      NewWalletBackendParams (..), WalletBackendParams (..),
                      WalletStartupOptions (..), getWalletNodeOptions,
                      walletDbPath, walletFlushDb, walletRebuildDb)
+import qualified Cardano.Wallet.Server.LegacyPlugins as LegacyPlugins
 import qualified Cardano.Wallet.Server.Plugins as Plugins
 import           Cardano.Wallet.WalletLayer (PassiveWalletLayer)
 import qualified Cardano.Wallet.WalletLayer.Kernel as WalletLayer.Kernel
@@ -103,15 +104,15 @@ actionWithWallet pm txpConfig sscParams nodeParams ntpConfig wArgs@WalletBackend
         keys' <- mapM getKeyById addrs
         forM_ keys' (syncWallet . keyToWalletDecrCredentials)
 
-    plugins :: TVar NtpStatus -> Plugins.Plugin WalletWebMode
+    plugins :: TVar NtpStatus -> LegacyPlugins.Plugin WalletWebMode
     plugins ntpStatus =
-        mconcat [ Plugins.conversation wArgs
-                , Plugins.legacyWalletBackend pm txpConfig wArgs ntpStatus
-                , Plugins.walletDocumentation wArgs
-                , Plugins.acidCleanupWorker wArgs
-                , Plugins.syncWalletWorker
-                , Plugins.resubmitterPlugin pm txpConfig
-                , Plugins.notifierPlugin
+        mconcat [ LegacyPlugins.conversation wArgs
+                , LegacyPlugins.legacyWalletBackend pm txpConfig wArgs ntpStatus
+                , LegacyPlugins.walletDocumentation wArgs
+                , LegacyPlugins.acidCleanupWorker wArgs
+                , LegacyPlugins.syncWalletWorker
+                , LegacyPlugins.resubmitterPlugin pm txpConfig
+                , LegacyPlugins.notifierPlugin
                 ]
 
 actionWithNewWallet :: (HasConfigurations, HasCompileInfo)
@@ -152,11 +153,24 @@ actionWithNewWallet pm txpConfig sscParams nodeParams ntpConfig params =
         -> (Diffusion Kernel.Mode.WalletMode -> Kernel.Mode.WalletMode ())
     runNodeWithInit w nr = runNode pm txpConfig nr (plugins w)
 
-    -- TODO: Don't know if we need any of the other plugins that are used
-    -- in the legacy wallet (see 'actionWithWallet').
+    -- FIXME: Do we need the monitoring API ?
     plugins :: (PassiveWalletLayer IO, PassiveWallet)
             -> Plugins.Plugin Kernel.Mode.WalletMode
-    plugins w = mconcat [ Plugins.walletBackend pm params w ]
+    plugins w = mconcat
+        -- The actual wallet backend server.
+        [ Plugins.apiServer pm params w
+
+        -- The corresponding wallet documention, served as a different
+        -- server which doesn't require client x509 certificates to
+        -- connect, but still serves the doc through TLS
+        , Plugins.docServer params
+
+        -- Periodically compact & snapshot the acid-state database.
+        , Plugins.acidStateSnapshots
+
+        -- | A @Plugin@ to notify frontend via websockets.
+        , Plugins.updateNotifier
+        ]
 
     -- Extract the logger name from node parameters
     --
@@ -168,6 +182,7 @@ actionWithNewWallet pm txpConfig sscParams nodeParams ntpConfig params =
       where
         loggerName :: LoggerName
         loggerName = lpDefaultName . bpLoggingParams . npBaseParams $ nodeParams
+
 
 -- | Runs an edge node plus its wallet backend API.
 startEdgeNode :: HasCompileInfo

wallet-new/src/Cardano/Wallet/Server/LegacyPlugins.hs
@@ -1,0 +1,226 @@
+{- | A collection of plugins used by this edge node.
+     A @Plugin@ is essentially a set of actions which will be run in
+     a particular monad, at some point in time.
+-}
+{-# LANGUAGE TupleSections #-}
+
+module Cardano.Wallet.Server.LegacyPlugins (
+      Plugin
+    , syncWalletWorker
+    , acidCleanupWorker
+    , conversation
+    , legacyWalletBackend
+    , walletDocumentation
+    , resubmitterPlugin
+    , notifierPlugin
+    ) where
+
+import           Universum
+
+import           Cardano.Wallet.API as API
+import           Cardano.Wallet.API.V1.Headers (applicationJson)
+import qualified Cardano.Wallet.API.V1.Types as V1
+import qualified Cardano.Wallet.LegacyServer as LegacyServer
+import           Cardano.Wallet.Server.CLI (RunMode, WalletBackendParams (..),
+                     isDebugMode, walletAcidInterval, walletDbOptions)
+import qualified Pos.Wallet.Web.Error.Types as V0
+
+import           Control.Exception (fromException)
+import           Data.Aeson
+import           Formatting (build, sformat, (%))
+import           Network.HTTP.Types.Status (badRequest400)
+import           Network.Wai (Application, Middleware, Response, responseLBS)
+import           Network.Wai.Handler.Warp (defaultSettings,
+                     setOnExceptionResponse)
+import           Network.Wai.Middleware.Cors (cors, corsMethods,
+                     corsRequestHeaders, simpleCorsResourcePolicy,
+                     simpleMethods)
+import           Ntp.Client (NtpStatus)
+import           Pos.Chain.Txp (TxpConfiguration)
+import           Pos.Infra.Diffusion.Types (Diffusion (..))
+import           Pos.Wallet.Web (cleanupAcidStatePeriodically)
+import           Pos.Wallet.Web.Pending.Worker (startPendingTxsResubmitter)
+import qualified Pos.Wallet.Web.Server.Runner as V0
+import           Pos.Wallet.Web.Sockets (getWalletWebSockets,
+                     upgradeApplicationWS)
+import qualified Servant
+import           System.Wlog (logInfo, modifyLoggerName, usingLoggerName)
+
+import           Pos.Context (HasNodeContext)
+import           Pos.Crypto (ProtocolMagic)
+import           Pos.Util (lensOf)
+
+import           Cardano.NodeIPC (startNodeJsIPC)
+import           Pos.Configuration (walletProductionApi,
+                     walletTxCreationDisabled)
+import           Pos.Infra.Shutdown.Class (HasShutdownContext (shutdownContext))
+import           Pos.Launcher.Configuration (HasConfigurations)
+import           Pos.Util.CompileInfo (HasCompileInfo)
+import           Pos.Wallet.Web.Mode (WalletWebMode)
+import           Pos.Wallet.Web.Server.Launcher (walletDocumentationImpl,
+                     walletServeImpl)
+import           Pos.Wallet.Web.State (askWalletDB)
+import           Pos.Wallet.Web.Tracking.Sync (processSyncRequest)
+import           Pos.Wallet.Web.Tracking.Types (SyncQueue)
+import           Pos.Web (serveWeb)
+import           Pos.WorkMode (WorkMode)
+
+
+-- A @Plugin@ running in the monad @m@.
+type Plugin m = [Diffusion m -> m ()]
+
+-- | A @Plugin@ to periodically compact & snapshot the acid-state database.
+acidCleanupWorker :: WalletBackendParams
+                  -> Plugin WalletWebMode
+acidCleanupWorker WalletBackendParams{..} = pure $ const $
+    modifyLoggerName (const "acidcleanup") $
+    askWalletDB >>= \db -> cleanupAcidStatePeriodically db (walletAcidInterval walletDbOptions)
+
+-- | The @Plugin@ which defines part of the conversation protocol for this node.
+conversation :: HasConfigurations => WalletBackendParams -> Plugin WalletWebMode
+conversation wArgs = map const (pluginsMonitoringApi wArgs)
+  where
+    pluginsMonitoringApi :: (WorkMode ctx m , HasNodeContext ctx)
+                         => WalletBackendParams
+                         -> [m ()]
+    pluginsMonitoringApi WalletBackendParams {..}
+        | enableMonitoringApi = [serveWeb monitoringApiPort walletTLSParams]
+        | otherwise = []
+
+walletDocumentation
+    :: (HasConfigurations, HasCompileInfo)
+    => WalletBackendParams
+    -> Plugin WalletWebMode
+walletDocumentation WalletBackendParams {..} = pure $ \_ ->
+    walletDocumentationImpl
+        application
+        walletDocAddress
+        tls
+        (Just defaultSettings)
+        Nothing
+  where
+    application :: WalletWebMode Application
+    application = do
+        let app = Servant.serve API.walletDocAPI LegacyServer.walletDocServer
+        return $ withMiddleware walletRunMode app
+    tls =
+        if isDebugMode walletRunMode then Nothing else walletTLSParams
+
+-- | A @Plugin@ to start the wallet backend API.
+legacyWalletBackend :: (HasConfigurations, HasCompileInfo)
+                    => ProtocolMagic
+                    -> TxpConfiguration
+                    -> WalletBackendParams
+                    -> TVar NtpStatus
+                    -> Plugin WalletWebMode
+legacyWalletBackend pm txpConfig WalletBackendParams {..} ntpStatus = pure $ \diffusion -> do
+    modifyLoggerName (const "legacyServantBackend") $ do
+      logInfo $ sformat ("Production mode for API: "%build)
+        walletProductionApi
+      logInfo $ sformat ("Transaction submission disabled: "%build)
+        walletTxCreationDisabled
+
+      ctx <- view shutdownContext
+      let
+        portCallback :: Word16 -> IO ()
+        portCallback port = usingLoggerName "NodeIPC" $ flip runReaderT ctx $ startNodeJsIPC port
+      walletServeImpl
+        (getApplication diffusion)
+        walletAddress
+        -- Disable TLS if in debug mode.
+        (if isDebugMode walletRunMode then Nothing else walletTLSParams)
+        (Just $ setOnExceptionResponse exceptionHandler defaultSettings)
+        (Just portCallback)
+  where
+    -- Gets the Wai `Application` to run.
+    getApplication :: Diffusion WalletWebMode -> WalletWebMode Application
+    getApplication diffusion = do
+        logInfo "Wallet Web API has STARTED!"
+        wsConn <- getWalletWebSockets
+        ctx <- V0.walletWebModeContext
+        return
+            $ withMiddleware walletRunMode
+            $ upgradeApplicationWS wsConn
+            $ Servant.serve API.walletAPI
+            $ LegacyServer.walletServer
+                (V0.convertHandler ctx)
+                pm
+                txpConfig
+                diffusion
+                ntpStatus
+                walletRunMode
+
+    exceptionHandler :: SomeException -> Response
+    exceptionHandler se =
+        case asum [handleV1Errors se, handleV0Errors se] of
+             Nothing -> handleGenericError se
+             Just r  -> r
+
+    -- Handles domain-specific errors coming from the V1 API.
+    handleV1Errors :: SomeException -> Maybe Response
+    handleV1Errors se =
+        let reify (we :: V1.WalletError) =
+                responseLBS (V1.toHttpErrorStatus we) [applicationJson] .  encode $ we
+        in fmap reify (fromException se)
+
+    -- Handles domain-specific errors coming from the V0 API, but rewraps it
+    -- into a jsend payload. It doesn't explicitly handle 'InternalError' or
+    -- 'DecodeError', as they can come from any part of the stack or even
+    -- rewrap some other exceptions (cfr 'rewrapToWalletError').
+    -- Uses the 'Buildable' istance on 'WalletError' to exploit any
+    -- available rendering and information-masking improvements.
+    handleV0Errors :: SomeException -> Maybe Response
+    handleV0Errors se =
+        let maskSensitive err =
+                case err of
+                    V0.RequestError _         -> err
+                    V0.InternalError _        -> V0.RequestError "InternalError"
+                    V0.DecodeError _          -> V0.RequestError "DecodeError"
+                    V0.DuplicateWalletError _ -> V0.RequestError "DuplicateWalletError"
+                    V0.NoSuchWalletError _    -> V0.RequestError "NoSuchWalletError"
+            reify :: V0.WalletError -> V1.WalletError
+            reify = V1.UnknownError . sformat build . maskSensitive
+        in fmap (responseLBS badRequest400 [applicationJson] .  encode . reify) (fromException se)
+
+    -- Handles any generic error, trying to prevent internal exceptions from leak outside.
+    handleGenericError :: SomeException -> Response
+    handleGenericError _ =
+        let
+            unknownV1Error = V1.UnknownError "Something went wrong."
+        in
+            responseLBS badRequest400 [applicationJson] $ encode unknownV1Error
+
+
+-- | A @Plugin@ to resubmit pending transactions.
+resubmitterPlugin :: HasConfigurations
+                  => ProtocolMagic
+                  -> TxpConfiguration
+                  -> Plugin WalletWebMode
+resubmitterPlugin pm txpConfig = [\diffusion -> askWalletDB >>= \db ->
+                        startPendingTxsResubmitter pm txpConfig db (sendTx diffusion)]
+
+-- | A @Plugin@ to notify frontend via websockets.
+notifierPlugin :: HasConfigurations => Plugin WalletWebMode
+notifierPlugin = [const V0.notifierPlugin]
+
+-- | The @Plugin@ responsible for the restoration & syncing of a wallet.
+syncWalletWorker :: HasConfigurations => Plugin WalletWebMode
+syncWalletWorker = pure $ const $
+    modifyLoggerName (const "syncWalletWorker") $
+    (view (lensOf @SyncQueue) >>= processSyncRequest)
+
+-- | "Attaches" the middleware to this 'Application', if any.
+-- When running in debug mode, chances are we want to at least allow CORS to test the API
+-- with a Swagger editor, locally.
+withMiddleware :: RunMode -> Application -> Application
+withMiddleware wrm app
+  | isDebugMode wrm = corsMiddleware app
+  | otherwise = app
+
+corsMiddleware :: Middleware
+corsMiddleware = cors (const $ Just policy)
+    where
+      policy = simpleCorsResourcePolicy
+        { corsRequestHeaders = ["Content-Type"]
+        , corsMethods = "PUT" : simpleMethods
+        }

input-output-hk/cardano-wallet-legacy

server/Main.hs
@@ -50,6 +50,7 @@ import           Cardano.Wallet.Server.CLI (ChooseWalletBackend (..),
                      WalletStartupOptions (..), getWalletDbOptions,
                      getWalletNodeOptions, walletDbPath, walletFlushDb,
                      walletRebuildDb)
+import qualified Cardano.Wallet.Server.LegacyPlugins as LegacyPlugins
 import qualified Cardano.Wallet.Server.Plugins as Plugins
 import           Cardano.Wallet.WalletLayer (PassiveWalletLayer)
 import qualified Cardano.Wallet.WalletLayer.Kernel as WalletLayer.Kernel
@@ -102,15 +103,15 @@ actionWithLegacyWallet coreConfig txpConfig sscParams nodeParams ntpConfig wArgs
         keys' <- mapM getKeyById addrs
         forM_ keys' (syncWallet . keyToWalletDecrCredentials)
 
-    plugins :: TVar NtpStatus -> Plugins.Plugin WalletWebMode
+    plugins :: TVar NtpStatus -> LegacyPlugins.Plugin WalletWebMode
     plugins ntpStatus =
-        mconcat [ Plugins.conversation wArgs
-                , Plugins.legacyWalletBackend coreConfig txpConfig wArgs ntpStatus
-                , Plugins.walletDocumentation wArgs
-                , Plugins.acidCleanupWorker wArgs
-                , Plugins.syncWalletWorker (configBlkSecurityParam coreConfig)
-                , Plugins.resubmitterPlugin coreConfig txpConfig
-                , Plugins.notifierPlugin
+        mconcat [ LegacyPlugins.conversation wArgs
+                , LegacyPlugins.legacyWalletBackend coreConfig txpConfig wArgs ntpStatus
+                , LegacyPlugins.walletDocumentation wArgs
+                , LegacyPlugins.acidCleanupWorker wArgs
+                , LegacyPlugins.syncWalletWorker (configBlkSecurityParam coreConfig)
+                , LegacyPlugins.resubmitterPlugin coreConfig txpConfig
+                , LegacyPlugins.notifierPlugin
                 ]
 
 -- | The "workhorse" responsible for starting a Cardano edge node plus a number of extra plugins.
@@ -162,11 +163,24 @@ actionWithWallet coreConfig txpConfig sscParams nodeParams ntpConfig params =
         -> (Diffusion Kernel.Mode.WalletMode -> Kernel.Mode.WalletMode ())
     runNodeWithInit w nr = runNode coreConfig txpConfig nr (plugins w)
 
-    -- TODO: Don't know if we need any of the other plugins that are used
-    -- in the legacy wallet (see 'actionWithLegacyWallet').
+    -- FIXME: Do we need the monitoring API ?
     plugins :: (PassiveWalletLayer IO, PassiveWallet)
             -> Plugins.Plugin Kernel.Mode.WalletMode
-    plugins w = mconcat [ Plugins.walletBackend pm params w ]
+    plugins w = mconcat
+        -- The actual wallet backend server.
+        [ Plugins.apiServer pm params w
+
+        -- The corresponding wallet documention, served as a different
+        -- server which doesn't require client x509 certificates to
+        -- connect, but still serves the doc through TLS
+        , Plugins.docServer params
+
+        -- Periodically compact & snapshot the acid-state database.
+        , Plugins.acidStateSnapshots
+
+        -- | A @Plugin@ to notify frontend via websockets.
+        , Plugins.updateNotifier
+        ]
 
     -- Extract the logger name from node parameters
     --
@@ -178,6 +192,7 @@ actionWithWallet coreConfig txpConfig sscParams nodeParams ntpConfig params =
       where
         loggerName :: LoggerName
         loggerName = lpDefaultName . bpLoggingParams . npBaseParams $ nodeParams
+
 
 -- | Runs an edge node plus its wallet backend API.
 startEdgeNode :: HasCompileInfo => WalletStartupOptions -> IO ()

src/Cardano/Wallet/Server/LegacyPlugins.hs
@@ -1,0 +1,226 @@
+{- | A collection of plugins used by this edge node.
+     A @Plugin@ is essentially a set of actions which will be run in
+     a particular monad, at some point in time.
+-}
+{-# LANGUAGE TupleSections #-}
+
+module Cardano.Wallet.Server.LegacyPlugins (
+      Plugin
+    , syncWalletWorker
+    , acidCleanupWorker
+    , conversation
+    , legacyWalletBackend
+    , walletDocumentation
+    , resubmitterPlugin
+    , notifierPlugin
+    ) where
+
+import           Universum
+
+import           Cardano.Wallet.API as API
+import           Cardano.Wallet.API.V1.Headers (applicationJson)
+import qualified Cardano.Wallet.API.V1.Types as V1
+import qualified Cardano.Wallet.LegacyServer as LegacyServer
+import           Cardano.Wallet.Server.CLI (RunMode, WalletBackendParams (..),
+                     isDebugMode, walletAcidInterval, walletDbOptions)
+import qualified Pos.Wallet.Web.Error.Types as V0
+
+import           Control.Exception (fromException)
+import           Data.Aeson
+import           Formatting (build, sformat, (%))
+import           Network.HTTP.Types.Status (badRequest400)
+import           Network.Wai (Application, Middleware, Response, responseLBS)
+import           Network.Wai.Handler.Warp (defaultSettings,
+                     setOnExceptionResponse)
+import           Network.Wai.Middleware.Cors (cors, corsMethods,
+                     corsRequestHeaders, simpleCorsResourcePolicy,
+                     simpleMethods)
+import           Ntp.Client (NtpStatus)
+import           Pos.Chain.Txp (TxpConfiguration)
+import           Pos.Infra.Diffusion.Types (Diffusion (..))
+import           Pos.Wallet.Web (cleanupAcidStatePeriodically)
+import           Pos.Wallet.Web.Pending.Worker (startPendingTxsResubmitter)
+import qualified Pos.Wallet.Web.Server.Runner as V0
+import           Pos.Wallet.Web.Sockets (getWalletWebSockets,
+                     upgradeApplicationWS)
+import qualified Servant
+
+import           Pos.Context (HasNodeContext)
+import           Pos.Core as Core (BlockCount, Config)
+import           Pos.Util (lensOf)
+import           Pos.Util.Wlog (logInfo, modifyLoggerName, usingLoggerName)
+
+import           Cardano.NodeIPC (startNodeJsIPC)
+import           Pos.Configuration (walletProductionApi,
+                     walletTxCreationDisabled)
+import           Pos.Infra.Shutdown.Class (HasShutdownContext (shutdownContext))
+import           Pos.Launcher.Configuration (HasConfigurations)
+import           Pos.Util.CompileInfo (HasCompileInfo)
+import           Pos.Wallet.Web.Mode (WalletWebMode)
+import           Pos.Wallet.Web.Server.Launcher (walletDocumentationImpl,
+                     walletServeImpl)
+import           Pos.Wallet.Web.State (askWalletDB)
+import           Pos.Wallet.Web.Tracking.Sync (processSyncRequest)
+import           Pos.Wallet.Web.Tracking.Types (SyncQueue)
+import           Pos.Web (serveWeb)
+import           Pos.WorkMode (WorkMode)
+
+
+-- A @Plugin@ running in the monad @m@.
+type Plugin m = [Diffusion m -> m ()]
+
+-- | A @Plugin@ to periodically compact & snapshot the acid-state database.
+acidCleanupWorker :: WalletBackendParams
+                  -> Plugin WalletWebMode
+acidCleanupWorker WalletBackendParams{..} = pure $ const $
+    modifyLoggerName (const "acidcleanup") $
+    askWalletDB >>= \db -> cleanupAcidStatePeriodically db (walletAcidInterval walletDbOptions)
+
+-- | The @Plugin@ which defines part of the conversation protocol for this node.
+conversation :: HasConfigurations => WalletBackendParams -> Plugin WalletWebMode
+conversation wArgs = map const (pluginsMonitoringApi wArgs)
+  where
+    pluginsMonitoringApi :: (WorkMode ctx m , HasNodeContext ctx)
+                         => WalletBackendParams
+                         -> [m ()]
+    pluginsMonitoringApi WalletBackendParams {..}
+        | enableMonitoringApi = [serveWeb monitoringApiPort walletTLSParams]
+        | otherwise = []
+
+walletDocumentation
+    :: (HasConfigurations, HasCompileInfo)
+    => WalletBackendParams
+    -> Plugin WalletWebMode
+walletDocumentation WalletBackendParams {..} = pure $ \_ ->
+    walletDocumentationImpl
+        application
+        walletDocAddress
+        tls
+        (Just defaultSettings)
+        Nothing
+  where
+    application :: WalletWebMode Application
+    application = do
+        let app = Servant.serve API.walletDocAPI LegacyServer.walletDocServer
+        return $ withMiddleware walletRunMode app
+    tls =
+        if isDebugMode walletRunMode then Nothing else walletTLSParams
+
+-- | A @Plugin@ to start the wallet backend API.
+legacyWalletBackend :: (HasConfigurations, HasCompileInfo)
+                    => Core.Config
+                    -> TxpConfiguration
+                    -> WalletBackendParams
+                    -> TVar NtpStatus
+                    -> Plugin WalletWebMode
+legacyWalletBackend coreConfig txpConfig WalletBackendParams {..} ntpStatus = pure $ \diffusion -> do
+    modifyLoggerName (const "legacyServantBackend") $ do
+      logInfo $ sformat ("Production mode for API: "%build)
+        walletProductionApi
+      logInfo $ sformat ("Transaction submission disabled: "%build)
+        walletTxCreationDisabled
+
+      ctx <- view shutdownContext
+      let
+        portCallback :: Word16 -> IO ()
+        portCallback port = usingLoggerName "NodeIPC" $ flip runReaderT ctx $ startNodeJsIPC port
+      walletServeImpl
+        (getApplication diffusion)
+        walletAddress
+        -- Disable TLS if in debug mode.
+        (if isDebugMode walletRunMode then Nothing else walletTLSParams)
+        (Just $ setOnExceptionResponse exceptionHandler defaultSettings)
+        (Just portCallback)
+  where
+    -- Gets the Wai `Application` to run.
+    getApplication :: Diffusion WalletWebMode -> WalletWebMode Application
+    getApplication diffusion = do
+        logInfo "Wallet Web API has STARTED!"
+        wsConn <- getWalletWebSockets
+        ctx <- V0.walletWebModeContext
+        return
+            $ withMiddleware walletRunMode
+            $ upgradeApplicationWS wsConn
+            $ Servant.serve API.walletAPI
+            $ LegacyServer.walletServer
+                (V0.convertHandler ctx)
+                coreConfig
+                txpConfig
+                diffusion
+                ntpStatus
+                walletRunMode
+
+    exceptionHandler :: SomeException -> Response
+    exceptionHandler se =
+        case asum [handleV1Errors se, handleV0Errors se] of
+             Nothing -> handleGenericError se
+             Just r  -> r
+
+    -- Handles domain-specific errors coming from the V1 API.
+    handleV1Errors :: SomeException -> Maybe Response
+    handleV1Errors se =
+        let reify (we :: V1.WalletError) =
+                responseLBS (V1.toHttpErrorStatus we) [applicationJson] .  encode $ we
+        in fmap reify (fromException se)
+
+    -- Handles domain-specific errors coming from the V0 API, but rewraps it
+    -- into a jsend payload. It doesn't explicitly handle 'InternalError' or
+    -- 'DecodeError', as they can come from any part of the stack or even
+    -- rewrap some other exceptions (cfr 'rewrapToWalletError').
+    -- Uses the 'Buildable' istance on 'WalletError' to exploit any
+    -- available rendering and information-masking improvements.
+    handleV0Errors :: SomeException -> Maybe Response
+    handleV0Errors se =
+        let maskSensitive err =
+                case err of
+                    V0.RequestError _         -> err
+                    V0.InternalError _        -> V0.RequestError "InternalError"
+                    V0.DecodeError _          -> V0.RequestError "DecodeError"
+                    V0.DuplicateWalletError _ -> V0.RequestError "DuplicateWalletError"
+                    V0.NoSuchWalletError _    -> V0.RequestError "NoSuchWalletError"
+            reify :: V0.WalletError -> V1.WalletError
+            reify = V1.UnknownError . sformat build . maskSensitive
+        in fmap (responseLBS badRequest400 [applicationJson] .  encode . reify) (fromException se)
+
+    -- Handles any generic error, trying to prevent internal exceptions from leak outside.
+    handleGenericError :: SomeException -> Response
+    handleGenericError _ =
+        let
+            unknownV1Error = V1.UnknownError "Something went wrong."
+        in
+            responseLBS badRequest400 [applicationJson] $ encode unknownV1Error
+
+
+-- | A @Plugin@ to resubmit pending transactions.
+resubmitterPlugin :: HasConfigurations
+                  => Core.Config
+                  -> TxpConfiguration
+                  -> Plugin WalletWebMode
+resubmitterPlugin coreConfig txpConfig = [\diffusion -> askWalletDB >>= \db ->
+                        startPendingTxsResubmitter coreConfig txpConfig db (sendTx diffusion)]
+
+-- | A @Plugin@ to notify frontend via websockets.
+notifierPlugin :: HasConfigurations => Plugin WalletWebMode
+notifierPlugin = [const V0.notifierPlugin]
+
+-- | The @Plugin@ responsible for the restoration & syncing of a wallet.
+syncWalletWorker :: HasConfigurations => BlockCount -> Plugin WalletWebMode
+syncWalletWorker k = pure $ const $
+    modifyLoggerName (const "syncWalletWorker") $
+    (view (lensOf @SyncQueue) >>= processSyncRequest k)
+
+-- | "Attaches" the middleware to this 'Application', if any.
+-- When running in debug mode, chances are we want to at least allow CORS to test the API
+-- with a Swagger editor, locally.
+withMiddleware :: RunMode -> Application -> Application
+withMiddleware wrm app
+  | isDebugMode wrm = corsMiddleware app
+  | otherwise = app
+
+corsMiddleware :: Middleware
+corsMiddleware = cors (const $ Just policy)
+    where
+      policy = simpleCorsResourcePolicy
+        { corsRequestHeaders = ["Content-Type"]
+        , corsMethods = "PUT" : simpleMethods
+        }