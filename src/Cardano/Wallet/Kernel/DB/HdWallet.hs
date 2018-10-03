nguyenquanghung010887/cardano-sl

[CBR-442] Replace Maybe with Strict.Maybe for DB types
This commit replaces all the occurrencies of Maybe types used in the DB
with their strict variant, to make sure that our DB stays nice and lean.

wallet-new/src/Cardano/Wallet/Kernel/DB/HdWallet.hs
@@ -90,7 +90,7 @@ module Cardano.Wallet.Kernel.DB.HdWallet (
 
 import           Universum hiding ((:|))
 
-import           Control.Lens (at, (+~), _Wrapped)
+import           Control.Lens (at, lazy, (+~), _Wrapped)
 import           Control.Lens.TH (makeLenses)
 import qualified Data.ByteString as BS
 import qualified Data.IxSet.Typed as IxSet (Indexable (..))
@@ -397,8 +397,8 @@ finishRestoration (SecurityParameter k) (HdAccountIncomplete (Checkpoints partia
       Nothing ->
         HdAccountUpToDate $ Checkpoints $ takeNewest k $ NewestFirst $
           (mostRecentHistorical :| olderHistorical)
-      Just secondLast | Just context <- secondLast ^. pcheckpointContext ->
-        if context `blockContextSucceeds` (mostRecentHistorical ^. checkpointContext)
+      Just secondLast | Just context <- secondLast ^. pcheckpointContext . lazy ->
+        if context `blockContextSucceeds` (mostRecentHistorical ^. checkpointContext . lazy)
           then HdAccountUpToDate $ Checkpoints $ takeNewest k $ NewestFirst $
                  SNE.prependList
                    (mkFull <$> initPartial)

input-output-hk/cardano-wallet-legacy

src/Cardano/Wallet/Kernel/DB/HdWallet.hs
@@ -93,7 +93,7 @@ module Cardano.Wallet.Kernel.DB.HdWallet (
 
 import           Universum hiding ((:|))
 
-import           Control.Lens (Getter, at, to, (+~), _Wrapped)
+import           Control.Lens (Getter, at, lazy, to, (+~), _Wrapped)
 import           Control.Lens.TH (makeLenses)
 import qualified Data.ByteString as BS
 import qualified Data.IxSet.Typed as IxSet (Indexable (..))
@@ -401,7 +401,7 @@ finishRestoration (SecurityParameter k) (HdAccountIncomplete (Checkpoints partia
         HdAccountUpToDate $ Checkpoints $ takeNewest k $ NewestFirst $
           (mostRecentHistorical :| olderHistorical)
       Just secondLast -> let checkpoint = secondLast ^. pcheckpointContext in
-        if checkpoint `blockContextSucceeds` (mostRecentHistorical ^. checkpointContext)
+        if checkpoint `blockContextSucceeds` (mostRecentHistorical ^. checkpointContext . lazy)
           then HdAccountUpToDate $ Checkpoints $ takeNewest k $ NewestFirst $
                  SNE.prependList
                    (mkFull <$> initPartial)