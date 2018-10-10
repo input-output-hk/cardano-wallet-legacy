Gyoung/cardano-sl

[CO-391] API V1 WIP-handlers for external transactions: createUnsignedTx.

wallet-new/src/Cardano/Wallet/WalletLayer.hs
@@ -22,6 +22,7 @@ module Cardano.Wallet.WalletLayer
     , DeleteAccountError(..)
     , UpdateAccountError(..)
     , ImportWalletError(..)
+    , NewUnsignedTransactionError(..)
     ) where
 
 import           Universum
@@ -43,8 +44,8 @@ import           Cardano.Wallet.API.Request.Filter (FilterOperations (..))
 import           Cardano.Wallet.API.Request.Sort (SortOperations (..))
 import           Cardano.Wallet.API.Response (SliceOf (..), WalletResponse)
 import           Cardano.Wallet.API.V1.Types (Account, AccountBalance,
-                     AccountIndex, AccountUpdate, Address, ForceNtpCheck,
-                     NewAccount, NewAddress, NewWallet, NodeInfo, NodeSettings,
+                     AccountIndex, AccountUpdate, Address, AddressLevel,
+                     ForceNtpCheck, NewAccount, NewAddress, NewWallet, NodeInfo, NodeSettings,
                      PasswordUpdate, Payment, Redemption, SpendingPassword,
                      Transaction, V1 (..), Wallet, WalletAddress, WalletId,
                      WalletImport, WalletUpdate)
@@ -485,6 +486,25 @@ data ActiveWalletLayer m = ActiveWalletLayer {
                    -- The payment we need to perform.
                    -> m (Either EstimateFeesError Coin)
 
+      -- | Prepares unsigned transaction. Please note that this function does /not/
+      -- perform a payment, it just creates a new transaction which will be signed
+      -- and submitted to the blockchain later.
+      --
+      -- It returns transaction and list of the source addresses with corresponding
+      -- derivation paths. These addresses and paths will be used by third party to
+      -- provide a proof that it has a right to spend money from these addresses.
+    , createUnsignedTx :: InputGrouping
+                       -- An preference on how to group inputs during coin selection
+                       -> ExpenseRegulation
+                       -- Who pays the fee, if the sender or the receivers.
+                       -> Payment
+                       -- The payment we need to perform.
+                       -> m (Either NewUnsignedTransactionError
+                                    ( Tx
+                                    , NonEmpty (Address, [AddressLevel])
+                                    )
+                            )
+
       -- | Redeem ada
     , redeemAda :: Redemption -> m (Either RedeemAdaError (Tx, TxMeta))
 
@@ -563,3 +583,19 @@ instance Buildable RedeemAdaError where
         bprint ("RedeemAdaWalletIdDecodingFailed " % build) txt
     build (RedeemAdaInvalidRedemptionCode txt) =
         bprint ("RedeemAdaInvalidRedemptionCode " % build) txt
+
+data NewUnsignedTransactionError =
+      NewUnsignedTransactionError Kernel.NewTransactionError
+    | NewTransactionWalletIdDecodingFailed Text
+
+instance Show NewUnsignedTransactionError where
+    show = formatToString build
+
+instance Exception NewUnsignedTransactionError
+
+instance Buildable NewUnsignedTransactionError where
+    build (NewUnsignedTransactionError err) =
+        bprint ("NewUnsignedTransactionError " % build) err
+    build (NewTransactionWalletIdDecodingFailed txt) =
+        bprint ("NewTransactionWalletIdDecodingFailed " % build) txt
+

input-output-hk/cardano-wallet-legacy

src/Cardano/Wallet/WalletLayer.hs
@@ -22,6 +22,7 @@ module Cardano.Wallet.WalletLayer
     , DeleteAccountError(..)
     , UpdateAccountError(..)
     , ImportWalletError(..)
+    , NewUnsignedTransactionError(..)
     ) where
 
 import           Universum
@@ -485,6 +486,25 @@ data ActiveWalletLayer m = ActiveWalletLayer {
                    -- The payment we need to perform.
                    -> m (Either EstimateFeesError Coin)
 
+      -- | Prepares unsigned transaction. Please note that this function does /not/
+      -- perform a payment, it just creates a new transaction which will be signed
+      -- and submitted to the blockchain later.
+      --
+      -- It returns transaction and list of the source addresses with corresponding
+      -- derivation paths. These addresses and paths will be used by third party to
+      -- provide a proof that it has a right to spend money from these addresses.
+    , createUnsignedTx :: InputGrouping
+                       -- An preference on how to group inputs during coin selection
+                       -> ExpenseRegulation
+                       -- Who pays the fee, if the sender or the receivers.
+                       -> Payment
+                       -- The payment we need to perform.
+                       -> m (Either NewUnsignedTransactionError
+                                    ( Tx
+                                    , NonEmpty (Address, [Word32])
+                                    )
+                            )
+
       -- | Redeem ada
     , redeemAda :: Redemption -> m (Either RedeemAdaError (Tx, TxMeta))
 
@@ -563,3 +583,19 @@ instance Buildable RedeemAdaError where
         bprint ("RedeemAdaWalletIdDecodingFailed " % build) txt
     build (RedeemAdaInvalidRedemptionCode txt) =
         bprint ("RedeemAdaInvalidRedemptionCode " % build) txt
+
+data NewUnsignedTransactionError =
+      NewUnsignedTransactionError Kernel.NewTransactionError
+    | NewTransactionWalletIdDecodingFailed Text
+
+instance Show NewUnsignedTransactionError where
+    show = formatToString build
+
+instance Exception NewUnsignedTransactionError
+
+instance Buildable NewUnsignedTransactionError where
+    build (NewUnsignedTransactionError err) =
+        bprint ("NewUnsignedTransactionError " % build) err
+    build (NewTransactionWalletIdDecodingFailed txt) =
+        bprint ("NewTransactionWalletIdDecodingFailed " % build) txt
+