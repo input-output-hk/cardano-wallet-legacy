mygirl8893/cardano-sl

[CO-410] Introduce NetworkMagic

wallet-new/src/Cardano/Wallet/API/V1/Swagger.hs
@@ -26,6 +26,7 @@ import           Cardano.Wallet.API.V1.Types
 import           Cardano.Wallet.TypeLits (KnownSymbols (..))
 
 import           Pos.Chain.Update (SoftwareVersion)
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Util.CompileInfo (CompileTimeInfo, ctiGitRevision)
 import           Pos.Util.Servant (CustomQueryFlag, LoggingApi)
 import           Pos.Wallet.Web.Swagger.Instances.Schema ()
@@ -389,7 +390,7 @@ $errors
       { Core.addrRoot =
           Crypto.unsafeAbstractHash ("asdfasdf" :: String)
       , Core.addrAttributes =
-          Core.mkAttributes $ Core.AddrAttributes Nothing Core.BootstrapEraDistr
+          Core.mkAttributes $ Core.AddrAttributes Nothing Core.BootstrapEraDistr fixedNM
       , Core.addrType =
           Core.ATPubKey
       }
@@ -1101,3 +1102,6 @@ api (compileInfo, curSoftwareVersion) walletAPI mkDescription = toSwagger wallet
   & paths %~ (DELETE, "/api/internal/reset-wallet-state") `setDescription` resetWalletStateDescription
   & paths %~ (POST,   "/api/v1/transactions/fees")        `setDescription` estimateFeesDescription
   & paths %~ (GET,    "/api/v1/addresses/{address}")      `setDescription` getAddressDescription
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainStage

wallet-new/src/Cardano/Wallet/Kernel/DB/HdWallet.hs
@@ -108,6 +108,7 @@ import           Serokell.Util (listJson)
 
 import qualified Pos.Core as Core
 import           Pos.Core.Chrono (NewestFirst (..))
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import qualified Pos.Crypto as Core
 
 import           Cardano.Wallet.API.V1.Types (V1 (..))
@@ -203,7 +204,10 @@ deriveSafeCopy 1 'base ''HasSpendingPassword
 --
 -- TODO: This may well disappear as part of [CBR-325].
 eskToHdRootId :: Core.EncryptedSecretKey -> HdRootId
-eskToHdRootId = HdRootId . InDb . Core.makePubKeyAddressBoot . Core.encToPublic
+eskToHdRootId = HdRootId . InDb . (Core.makePubKeyAddressBoot fixedNM) . Core.encToPublic
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainStage
 
 
 {-------------------------------------------------------------------------------

wallet-new/src/Cardano/Wallet/Kernel/DB/InDb.hs
@@ -19,7 +19,7 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.List.NonEmpty as NEL
 import qualified Data.Map as Map
 import qualified Data.SafeCopy as SC
-import           Data.Serialize (Put)
+import           Data.Serialize (Put, getWord8, putWord8)
 import qualified Data.Set as Set
 import qualified Data.Time.Units
 import qualified Data.Vector as V
@@ -32,7 +32,9 @@ import qualified Pos.Chain.Txp as Txp
 import qualified Pos.Chain.Update as Core
 import qualified Pos.Core as Core
 import qualified Pos.Core.Attributes as Core
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import qualified Pos.Crypto as Core
+import           Pos.Util.Util (cerealError)
 
 import qualified Cardano.Crypto.Wallet as CCW
 import           UTxO.Util
@@ -106,10 +108,12 @@ instance SC.SafeCopy (InDb Core.AddrAttributes) where
     getCopy = SC.contain $ do
         yiap :: Maybe (InDb Core.HDAddressPayload) <- SC.safeGet
         InDb (ast :: Core.AddrStakeDistribution) <- SC.safeGet
-        pure (InDb (Core.AddrAttributes (fmap _fromDb yiap) ast))
-    putCopy (InDb (Core.AddrAttributes yap asr)) = SC.contain $ do
+        InDb (nm :: NetworkMagic) <- SC.safeGet
+        pure (InDb (Core.AddrAttributes (fmap _fromDb yiap) ast nm))
+    putCopy (InDb (Core.AddrAttributes yap asr nm)) = SC.contain $ do
         SC.safePut (fmap InDb yap)
         SC.safePut (InDb asr)
+        SC.safePut (InDb nm)
 
 instance SC.SafeCopy (InDb Core.AddrStakeDistribution) where
     getCopy = SC.contain $ fmap InDb $ do
@@ -794,6 +798,15 @@ instance SC.SafeCopy (InDb Core.RequiresNetworkMagic) where
     putCopy (InDb rnm) = SC.contain $ case rnm of
         Core.RequiresNoMagic -> SC.safePut (0 :: Word8)
         Core.RequiresMagic   -> SC.safePut (1 :: Word8)
+
+instance SC.SafeCopy (InDb NetworkMagic) where
+    getCopy = SC.contain $ getWord8 >>= \case
+        0 -> pure (InDb NetworkMainStage)
+        1 -> InDb . NetworkTestnet <$> SC.safeGet
+        t -> cerealError $ "getCopy@(InDb NetworkMagic): couldn't read tag: " <> show t
+
+    putCopy (InDb NetworkMainStage)   = SC.contain $ putWord8 0
+    putCopy (InDb (NetworkTestnet i)) = SC.contain $ putWord8 1 >> SC.safePut i
 
 instance SC.SafeCopy (InDb Core.GenesisProof) where
     getCopy = SC.contain $ do

wallet-new/src/Cardano/Wallet/Kernel/Transactions.hs
@@ -38,6 +38,7 @@ import           Pos.Chain.Txp (Tx (..), TxAux (..), TxId, TxIn (..),
 import qualified Pos.Client.Txp.Util as CTxp
 import           Pos.Core (Address, Coin, TxFeePolicy (..), unsafeSubCoin)
 import qualified Pos.Core as Core
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Crypto (EncryptedSecretKey, PassPhrase, RedeemSecretKey,
                      SafeSigner (..), ShouldCheckPassphrase (..), hash,
                      redeemToPublic)
@@ -458,7 +459,8 @@ mkSigner spendingPassword (Just esk) snapshot addr =
                                        . HD.hdAddressIdParent
                                        . HD.hdAccountIdIx
                                        . to HD.getHdAccountIx
-                res = Core.deriveLvl2KeyPair (Core.IsBootstrapEraAddr True)
+                res = Core.deriveLvl2KeyPair fixedNM
+                                             (Core.IsBootstrapEraAddr True)
                                              (ShouldCheckPassphrase False)
                                              spendingPassword
                                              esk
@@ -554,7 +556,7 @@ redeemAda w@ActiveWallet{..} accId pw rsk = runExceptT $ do
     return (taTx tx, meta)
   where
     redeemAddr :: Address
-    redeemAddr = Core.makeRedeemAddress $ redeemToPublic rsk
+    redeemAddr = Core.makeRedeemAddress fixedNM $ redeemToPublic rsk
 
     -- | Note: we use `getCreationTimestamp` provided by the `NodeStateAdaptor`
     --   to compute the createdAt timestamp for `TxMeta`
@@ -592,3 +594,6 @@ redeemAda w@ActiveWallet{..} accId pw rsk = runExceptT $ do
         isOutput (inp, TxOutAux (TxOut addr coin)) = do
             guard $ addr == redeemAddr
             return (inp, coin)
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainStage

wallet-new/test/unit/Test/Spec/GetTransactions.hs
@@ -27,6 +27,7 @@ import qualified Pos.Chain.Txp as Core
 import           Pos.Core as Core
 import           Pos.Core (Coin (..), IsBootstrapEraAddr (..),
                      deriveLvl2KeyPair, mkCoin)
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Crypto (EncryptedSecretKey, ShouldCheckPassphrase (..),
                      emptyPassphrase, safeDeterministicKeyGen)
 import           Pos.Crypto.HD (firstHardened)
@@ -110,7 +111,8 @@ prepareFixtures initialBalance = do
         utxo' <- foldlM (\acc (txIn, (TxOutAux (TxOut _ coin))) -> do
                             newIndex <- deriveIndex (pick . choose) HdAddressIx HardDerivation
 
-                            let Just (addr, _) = deriveLvl2KeyPair (IsBootstrapEraAddr True)
+                            let Just (addr, _) = deriveLvl2KeyPair fixedNM
+                                                                (IsBootstrapEraAddr True)
                                                                 (ShouldCheckPassphrase True)
                                                                 mempty
                                                                 esk
@@ -470,3 +472,6 @@ payAux aw hdAccountId payees fees = do
     bimap STB STB payRes `shouldSatisfy` isRight
     let Right t = payRes
     return t
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainStage

wallet-new/test/unit/Test/Spec/NewPayment.hs
@@ -26,6 +26,7 @@ import           Formatting (build, formatToString, sformat)
 import           Pos.Chain.Txp (TxOut (..), TxOutAux (..))
 import           Pos.Core (Address, Coin (..), IsBootstrapEraAddr (..),
                      deriveLvl2KeyPair, mkCoin)
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Crypto (EncryptedSecretKey, ShouldCheckPassphrase (..),
                      emptyPassphrase, safeDeterministicKeyGen)
 
@@ -98,7 +99,8 @@ prepareFixtures initialBalance toPay = do
     utxo' <- foldlM (\acc (txIn, (TxOutAux (TxOut _ coin))) -> do
                         newIndex <- deriveIndex (pick . choose) HdAddressIx HardDerivation
 
-                        let Just (addr, _) = deriveLvl2KeyPair (IsBootstrapEraAddr True)
+                        let Just (addr, _) = deriveLvl2KeyPair fixedNM
+                                                               (IsBootstrapEraAddr True)
                                                                (ShouldCheckPassphrase True)
                                                                mempty
                                                                esk
@@ -321,3 +323,6 @@ spec = describe "NewPayment" $ do
                         let pmt = newPayment { V1.pmtSpendingPassword = randomPass }
                         res <- liftIO (runExceptT . runHandler' $ Handlers.estimateFees activeLayer pmt)
                         liftIO ((bimap identity STB res) `shouldSatisfy` isRight)
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainStage

input-output-hk/cardano-wallet-legacy

src/Cardano/Wallet/API/V1/Swagger.hs
@@ -26,6 +26,7 @@ import           Cardano.Wallet.API.V1.Types
 import           Cardano.Wallet.TypeLits (KnownSymbols (..))
 
 import           Pos.Chain.Update (SoftwareVersion)
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Util.CompileInfo (CompileTimeInfo, ctiGitRevision)
 import           Pos.Util.Servant (CustomQueryFlag, LoggingApi)
 import           Pos.Wallet.Web.Swagger.Instances.Schema ()
@@ -392,7 +393,7 @@ $errors
       { Core.addrRoot =
           Crypto.unsafeAbstractHash ("asdfasdf" :: String)
       , Core.addrAttributes =
-          Core.mkAttributes $ Core.AddrAttributes Nothing Core.BootstrapEraDistr
+          Core.mkAttributes $ Core.AddrAttributes Nothing Core.BootstrapEraDistr fixedNM
       , Core.addrType =
           Core.ATPubKey
       }
@@ -1104,3 +1105,6 @@ api (compileInfo, curSoftwareVersion) walletAPI mkDescription = toSwagger wallet
   & paths %~ (DELETE, "/api/internal/reset-wallet-state") `setDescription` resetWalletStateDescription
   & paths %~ (POST,   "/api/v1/transactions/fees")        `setDescription` estimateFeesDescription
   & paths %~ (GET,    "/api/v1/addresses/{address}")      `setDescription` getAddressDescription
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainOrStage

src/Cardano/Wallet/Kernel/DB/HdWallet.hs
@@ -111,6 +111,7 @@ import           Serokell.Util (listJson)
 
 import qualified Pos.Core as Core
 import           Pos.Core.Chrono (NewestFirst (..))
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import qualified Pos.Crypto as Core
 
 import           Cardano.Wallet.API.V1.Types (V1 (..))
@@ -206,7 +207,10 @@ deriveSafeCopy 1 'base ''HasSpendingPassword
 --
 -- TODO: This may well disappear as part of [CBR-325].
 eskToHdRootId :: Core.EncryptedSecretKey -> HdRootId
-eskToHdRootId = HdRootId . InDb . Core.makePubKeyAddressBoot . Core.encToPublic
+eskToHdRootId = HdRootId . InDb . (Core.makePubKeyAddressBoot fixedNM) . Core.encToPublic
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainOrStage
 
 
 {-------------------------------------------------------------------------------

src/Cardano/Wallet/Kernel/DB/InDb.hs
@@ -19,7 +19,7 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.List.NonEmpty as NEL
 import qualified Data.Map as Map
 import qualified Data.SafeCopy as SC
-import           Data.Serialize (Put)
+import           Data.Serialize (Put, getWord8, putWord8)
 import qualified Data.Set as Set
 import qualified Data.Time.Units
 import qualified Data.Vector as V
@@ -32,7 +32,9 @@ import qualified Pos.Chain.Txp as Txp
 import qualified Pos.Chain.Update as Core
 import qualified Pos.Core as Core
 import qualified Pos.Core.Attributes as Core
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import qualified Pos.Crypto as Core
+import           Pos.Util.Util (cerealError)
 
 import qualified Cardano.Crypto.Wallet as CCW
 import           UTxO.Util
@@ -106,10 +108,12 @@ instance SC.SafeCopy (InDb Core.AddrAttributes) where
     getCopy = SC.contain $ do
         yiap :: Maybe (InDb Core.HDAddressPayload) <- SC.safeGet
         InDb (ast :: Core.AddrStakeDistribution) <- SC.safeGet
-        pure (InDb (Core.AddrAttributes (fmap _fromDb yiap) ast))
-    putCopy (InDb (Core.AddrAttributes yap asr)) = SC.contain $ do
+        InDb (nm :: NetworkMagic) <- SC.safeGet
+        pure (InDb (Core.AddrAttributes (fmap _fromDb yiap) ast nm))
+    putCopy (InDb (Core.AddrAttributes yap asr nm)) = SC.contain $ do
         SC.safePut (fmap InDb yap)
         SC.safePut (InDb asr)
+        SC.safePut (InDb nm)
 
 instance SC.SafeCopy (InDb Core.AddrStakeDistribution) where
     getCopy = SC.contain $ fmap InDb $ do
@@ -794,6 +798,15 @@ instance SC.SafeCopy (InDb Core.RequiresNetworkMagic) where
     putCopy (InDb rnm) = SC.contain $ case rnm of
         Core.RequiresNoMagic -> SC.safePut (0 :: Word8)
         Core.RequiresMagic   -> SC.safePut (1 :: Word8)
+
+instance SC.SafeCopy (InDb NetworkMagic) where
+    getCopy = SC.contain $ getWord8 >>= \case
+        0 -> pure (InDb NetworkMainOrStage)
+        1 -> InDb . NetworkTestnet <$> SC.safeGet
+        t -> cerealError $ "getCopy@(InDb NetworkMagic): couldn't read tag: " <> show t
+
+    putCopy (InDb NetworkMainOrStage)   = SC.contain $ putWord8 0
+    putCopy (InDb (NetworkTestnet i)) = SC.contain $ putWord8 1 >> SC.safePut i
 
 instance SC.SafeCopy (InDb Core.GenesisProof) where
     getCopy = SC.contain $ do

src/Cardano/Wallet/Kernel/Transactions.hs
@@ -74,6 +74,7 @@ import           Pos.Chain.Txp as Core (TxAttributes, TxAux, TxIn, TxOut,
 import qualified Pos.Client.Txp.Util as CTxp
 import           Pos.Core (Address, Coin, TxFeePolicy (..), unsafeSubCoin)
 import qualified Pos.Core as Core
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Crypto (EncryptedSecretKey, PassPhrase, ProtocolMagic,
                      PublicKey, RedeemSecretKey, SafeSigner (..),
                      ShouldCheckPassphrase (..), Signature (..), hash,
@@ -597,7 +598,8 @@ mkSigner spendingPassword (Just esk) snapshot addr =
                                        . HD.hdAddressIdParent
                                        . HD.hdAccountIdIx
                                        . to HD.getHdAccountIx
-                res = Core.deriveLvl2KeyPair (Core.IsBootstrapEraAddr True)
+                res = Core.deriveLvl2KeyPair fixedNM
+                                             (Core.IsBootstrapEraAddr True)
                                              (ShouldCheckPassphrase False)
                                              spendingPassword
                                              esk
@@ -693,7 +695,7 @@ redeemAda w@ActiveWallet{..} accId pw rsk = runExceptT $ do
     return (taTx tx, meta)
   where
     redeemAddr :: Address
-    redeemAddr = Core.makeRedeemAddress $ redeemToPublic rsk
+    redeemAddr = Core.makeRedeemAddress fixedNM $ redeemToPublic rsk
 
     -- | Note: we use `getCreationTimestamp` provided by the `NodeStateAdaptor`
     --   to compute the createdAt timestamp for `TxMeta`
@@ -798,3 +800,6 @@ mkStdTx pm shuffle hdwSigners inps outs change = do
          -- 'TxOwnedInputs'.
         repack :: (Core.TxIn, Core.TxOutAux) -> (Core.TxOut, Core.TxIn)
         repack (txIn, aux) = (Core.toaOut aux, txIn)
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainOrStage

test/unit/Test/Spec/GetTransactions.hs
@@ -27,6 +27,7 @@ import qualified Pos.Chain.Txp as Core
 import           Pos.Core as Core
 import           Pos.Core (Coin (..), IsBootstrapEraAddr (..),
                      deriveLvl2KeyPair, mkCoin)
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Crypto (EncryptedSecretKey, ShouldCheckPassphrase (..),
                      emptyPassphrase, safeDeterministicKeyGen)
 import           Pos.Crypto.HD (firstHardened)
@@ -110,7 +111,8 @@ prepareFixtures initialBalance = do
         utxo' <- foldlM (\acc (txIn, (TxOutAux (TxOut _ coin))) -> do
                             newIndex <- deriveIndex (pick . choose) HdAddressIx HardDerivation
 
-                            let Just (addr, _) = deriveLvl2KeyPair (IsBootstrapEraAddr True)
+                            let Just (addr, _) = deriveLvl2KeyPair fixedNM
+                                                                (IsBootstrapEraAddr True)
                                                                 (ShouldCheckPassphrase True)
                                                                 mempty
                                                                 esk
@@ -470,3 +472,6 @@ payAux aw hdAccountId payees fees = do
     bimap STB STB payRes `shouldSatisfy` isRight
     let Right t = payRes
     return t
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainOrStage

test/unit/Test/Spec/NewPayment.hs
@@ -26,6 +26,7 @@ import           Formatting (build, formatToString, sformat)
 import           Pos.Chain.Txp (TxOut (..), TxOutAux (..))
 import           Pos.Core (Address, Coin (..), IsBootstrapEraAddr (..),
                      deriveLvl2KeyPair, mkCoin)
+import           Pos.Core.NetworkMagic (NetworkMagic (..))
 import           Pos.Crypto (EncryptedSecretKey, ShouldCheckPassphrase (..),
                      emptyPassphrase, safeDeterministicKeyGen)
 
@@ -98,7 +99,8 @@ prepareFixtures initialBalance toPay = do
     utxo' <- foldlM (\acc (txIn, (TxOutAux (TxOut _ coin))) -> do
                         newIndex <- deriveIndex (pick . choose) HdAddressIx HardDerivation
 
-                        let Just (addr, _) = deriveLvl2KeyPair (IsBootstrapEraAddr True)
+                        let Just (addr, _) = deriveLvl2KeyPair fixedNM
+                                                               (IsBootstrapEraAddr True)
                                                                (ShouldCheckPassphrase True)
                                                                mempty
                                                                esk
@@ -321,3 +323,6 @@ spec = describe "NewPayment" $ do
                         let pmt = newPayment { V1.pmtSpendingPassword = randomPass }
                         res <- liftIO (runExceptT . runHandler' $ Handlers.estimateFees activeLayer pmt)
                         liftIO ((bimap identity STB res) `shouldSatisfy` isRight)
+
+fixedNM :: NetworkMagic
+fixedNM = NetworkMainOrStage