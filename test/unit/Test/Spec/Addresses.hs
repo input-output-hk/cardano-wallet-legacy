5df816d2c8e23b5a746b21b2ac4ab56d1e307f89
Johannes Lund <johannes.lund@iohk.io>
2018-11-09 15:43:27 +0100

    Reduce more in Addresses

--- test/unit/Test/Spec/Addresses.hs
+++ test/unit/Test/Spec/Addresses.hs
@@ -204,7 +204,7 @@ spec :: Spec
 spec = describe "Addresses" $ do
     describe "CreateAddress" $ do
         describe "Address creation (wallet layer)" $ do
-            prop "works as expected in the happy path scenario" $ withMaxSuccess 200 $
+            prop "works as expected in the happy path scenario" $ withMaxSuccess 20 $
                 monadicIO $ do
                     pm <- pick arbitrary
                     withFixture pm $ \keystore layer _ Fixture{..} -> do
@@ -217,7 +217,7 @@ spec = describe "Addresses" $ do
                         (bimap STB STB res) `shouldSatisfy` isRight
 
         describe "Address creation (kernel)" $ do
-            prop "works as expected in the happy path scenario" $ withMaxSuccess 200 $
+            prop "works as expected in the happy path scenario" $ withMaxSuccess 20 $
                 monadicIO $ do
                     pm <- pick arbitrary
                     withFixture pm $ \keystore _ _ Fixture{..} -> do
@@ -399,7 +399,7 @@ spec = describe "Addresses" $ do
                                     -> pure ()
                            _ -> fail ("Got " ++ show res)
 
-            prop "arbitrary number of addresses, pages and per page" $ withMaxSuccess 500 $ do
+            prop "arbitrary number of addresses, pages and per page" $ withMaxSuccess 50 $ do
                 monadicIO $ do
                     (rNumOfAddresses :: Int) <- pick $ elements [0..15]
                     (rNumOfPages :: Int) <- pick $ elements [0..15]
@@ -432,7 +432,7 @@ spec = describe "Addresses" $ do
                             Right wr | null (wrData wr) -> pure ()
                             _ -> fail ("Got " ++ show res)
 
-            prop "it yields the correct number of results" $ withMaxSuccess 20 $ do
+            prop "it yields the correct number of results" $ withMaxSuccess 5 $ do
                 monadicIO $ do
                     pm <- pick arbitrary
                     withAddressesFixtures pm (DesiredNewAccs 3) (DesiredNewAddrs 4) $ \_ layer _ (_, total) -> do
@@ -444,7 +444,7 @@ spec = describe "Addresses" $ do
                                 length (wrData wr) `shouldBe` min 40 total
                             _        -> fail ("Got " ++ show res)
 
-            prop "is deterministic" $ withMaxSuccess 20 $ do
+            prop "is deterministic" $ withMaxSuccess 5 $ do
                 monadicIO $ do
                     pm <- pick arbitrary
                     withAddressesFixtures pm (DesiredNewAccs 3) (DesiredNewAddrs 8) $ \_ layer _ (_, expectedTotal) -> do
@@ -456,7 +456,7 @@ spec = describe "Addresses" $ do
                                               (,) <$> mkRequest pp <*> mkRequest pp
                             r1 `shouldBe` r2
 
-            prop "yields the correct set of resutls" $ withMaxSuccess 20 $ do
+            prop "yields the correct set of resutls" $ withMaxSuccess 5 $ do
                 monadicIO $ do
                     pm <- pick arbitrary
                     withAddressesFixtures pm (DesiredNewAccs 4) (DesiredNewAddrs 8) $ \_ layer _ (_, expectedTotal) -> do
@@ -482,7 +482,7 @@ spec = describe "Addresses" $ do
                                 (rootId <$> con) `shouldBe` (rootId <$> wrData wr)
                             _        -> fail ("Got " ++ show res)
 
-            prop "yields the correct ordered resutls when there is one account" $ withMaxSuccess 20 $ do
+            prop "yields the correct ordered resutls when there is one account" $ withMaxSuccess 5 $ do
                 monadicIO $ do
                     pm <- pick arbitrary
                     withAddressesFixtures pm (DesiredNewAccs 0) (DesiredNewAddrs 15) $ \_ layer _ (_, expectedTotal) -> do
@@ -509,7 +509,7 @@ spec = describe "Addresses" $ do
                             _        -> fail ("Got " ++ show res)
 
 
-            prop "yields the correct ordered resutls" $ withMaxSuccess 20 $ do
+            prop "yields the correct ordered resutls" $ withMaxSuccess 5 $ do
                 monadicIO $ do
                   pm <- pick arbitrary
                   forM_ [(DesiredNewAccs 4,DesiredNewAddrs 8),
@@ -538,7 +538,7 @@ spec = describe "Addresses" $ do
     describe "ValidateAddress" $ do
         describe "Address validation (wallet layer)" $ do
 
-            prop "works as expected in the happy path scenario (valid address, ours)" $ withMaxSuccess 25 $
+            prop "works as expected in the happy path scenario (valid address, ours)" $ withMaxSuccess 5 $
                 monadicIO $ do
                     pm <- pick arbitrary
                     withAddressFixtures pm 1 $ \_ layer _ [af] -> do
