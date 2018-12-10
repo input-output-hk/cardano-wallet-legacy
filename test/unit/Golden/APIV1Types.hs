fbb130b73f4513f2561072ed032d16b06d1cf655
Denis Shevchenko <denis.shevchenko@iohk.io>
2018-12-10 17:17:18 +0400

    issue119 Golden test for V1 PassPhrase.

---
+++ test/unit/Golden/APIV1Types.hs
@@ -0,0 +1,39 @@
+module Golden.APIV1Types
+   ( tests
+   ) where
+
+import           Universum
+
+import           Hedgehog (Property)
+import qualified Hedgehog as H
+
+import           Cardano.Wallet.API.V1.Types (V1 (..))
+import           Pos.Crypto (PassPhrase (..))
+
+import           Test.Pos.Util.Golden (discoverGolden, goldenTestJSON)
+
+-----------------------------------------------------------------------
+-- Main test export
+-----------------------------------------------------------------------
+
+tests :: IO Bool
+tests =
+    H.checkSequential $$discoverGolden
+
+-------------------------------------------------------------------------------
+-- API V1 ToJSON/FromJSON
+-------------------------------------------------------------------------------
+
+golden_WalletPassPhrase :: Property
+golden_WalletPassPhrase =
+    goldenTestJSON
+        (V1 examplePassPhrase)
+        "test/unit/Golden/golden/apiV1Types/json/PassPhrase"
+
+-------------------------------------------------------------------------------
+-- Examples
+-------------------------------------------------------------------------------
+
+-- | Currently PassPhrase should be 32 bytes long.
+examplePassPhrase :: PassPhrase
+examplePassPhrase = PassPhrase "hello_new_awesome_cardano_wallet"
