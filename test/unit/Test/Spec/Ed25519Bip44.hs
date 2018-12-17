06810b7d23e4c83b891dfbdea0f65cb4e8f049ee
Ante Kegalj <akegalj@gmail.com>
2018-12-17 13:14:24 +0100

    Fix address public key derivation
    This commit fixes work done by #31. Instead of diriving address
    public key dirrectly with account public key, bip44 specifies
    intermediate Change derivation. Simplified naming.
    https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#Change

--- test/unit/Test/Spec/Ed25519Bip44.hs
+++ test/unit/Test/Spec/Ed25519Bip44.hs
@@ -8,24 +8,27 @@ import           Universum
 
 import           Pos.Crypto (PublicKey)
 
-import           Cardano.Wallet.Kernel.Ed25519Bip44 (deriveLvl3PublicKey)
+import           Cardano.Wallet.Kernel.Ed25519Bip44 (ChangeChain,
+                     deriveAddressPublicKey)
 
 import           Test.Hspec (Spec, describe, it)
 import           Test.Pos.Core.Arbitrary ()
 import           Test.QuickCheck (Property, property)
 
 -- | It proves that we cannot derive address public key
--- if address index is too big.
-prop_cannotDerivelvl3PublicKeyForBigIx
+-- if address index is too big. We should be able to derive
+-- Address public key only with non-hardened address index
+prop_cannotDeriveAddressPublicKeyForBigIx
     :: PublicKey
+    -> ChangeChain
     -> Word32
     -> Property
-prop_cannotDerivelvl3PublicKeyForBigIx accountPublicKey addressIx = property $
+prop_cannotDeriveAddressPublicKeyForBigIx accountPublicKey change addressIx = property $
     if addressIx >= maxIx
         then isNothing result
         else isJust result
   where
-    result = deriveLvl3PublicKey accountPublicKey addressIx
+    result = deriveAddressPublicKey accountPublicKey change addressIx
     -- This is maximum value for soft derivation (only soft derivation
     -- is allowed to derive public key from public key).
     maxIx = 0x8000000
@@ -33,4 +36,4 @@ prop_cannotDerivelvl3PublicKeyForBigIx accountPublicKey addressIx = property $
 spec :: Spec
 spec = describe "Ed25519Bip44" $ do
     it "Derivation fails if address index is too big" $
-        property prop_cannotDerivelvl3PublicKeyForBigIx
+        property prop_cannotDeriveAddressPublicKeyForBigIx
