0dfd08100b9252eb5403d8c5879657fc0bc6508a
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-12-17 16:42:51 +1000

    [DEVOPS-1179] nix-tools: enable all cross-compiling modules and overlays

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -12,6 +12,10 @@ let
 
 in
 let
+  # save the nixpkgs value in pkgs'
+  # so we can work with `pkgs` provided by modules.
+  pkgs' = pkgs;
+
   # all packages from hackage as nix expressions
   hackage = import (overrideWith "hackage"
                     (pkgs.fetchFromGitHub { owner  = "angerman";
@@ -48,152 +52,131 @@ let
   #  packages.cbors.patches = [ ./one.patch ];
   #  packages.cbors.flags.optimize-gmp = false;
   #
-#   pkgSet = haskell.mkNewPkgSet {
-#     inherit pkgs;
-#     pkg-def = stackage.${stack-pkgs.resolver};
-#     pkg-def-overlays = [
-#       stack-pkgs.overlay
-#       # We use some customized libiserv/remote-iserv/iserv-proxy
-#       # instead of the ones provided by ghc. This is mostly due
-#       # to being able to hack on them freely as needed.
-#       #
-#       # iserv is only relevant for template-haskell execution in
-#       # a cross compiling setup.
-#       {
-#         ghci         = ./ghci.nix;
-#         ghc-boot     = ./ghc-boot.nix;
-#         libiserv     = ./libiserv.nix;
-#         remote-iserv = ./remote-iserv.nix;
-#         iserv-proxy  = ./iserv-proxy.nix;
-# #        packages.hfsevents.revision = import ../hfsevents-0.1.6;
-#       }
-#       (hackage: {
-#           hsc2hs = hackage.hsc2hs."0.68.4".revisions.default;
-#           # stackage 12.17 beautifully omitts the Win32 pkg
-#           Win32 = hackage.Win32."2.6.2.0".revisions.default;
-#       })
-#     ];
-#     modules = [
-#       {
-#          # This needs true, otherwise we miss most of the interesting
-#          # modules.
-#          packages.ghci.flags.ghci = true;
-#          # this needs to be true to expose module
-#          #  Message.Remote
-#          # as needed by libiserv.
-#          packages.libiserv.flags.network = true;
-
-#          # enable golden tests on cardano-crypto
-#          packages.cardano-crypto.flags.golden-tests = true;
-#       }
-#       ({ config, ... }: {
-#           packages.hsc2hs.components.exes.hsc2hs.doExactConfig= true;
-#           packages.Win32.components.library.build-tools = [ config.hsPkgs.buildPackages.hsc2hs ];
-# #          packages.Win32.components.library.doExactConfig = true;
-#           packages.remote-iserv.postInstall = ''
-#             cp ${pkgs.windows.mingw_w64_pthreads}/bin/libwinpthread-1.dll $out/bin/
-#           '';
-#       })
-#       {
-#         packages.conduit.patches            = [ ./patches/conduit-1.3.0.2.patch ];
-#         packages.cryptonite-openssl.patches = [ ./patches/cryptonite-openssl-0.7.patch ];
-#         packages.streaming-commons.patches  = [ ./patches/streaming-commons-0.2.0.0.patch ];
-#         packages.x509-system.patches        = [ ./patches/x509-system-1.6.6.patch ];
-#         packages.file-embed-lzma.patches    = [ ./patches/file-embed-lzma-0.patch ];
-#       }
-#       # cross compilation logic
-#       ({ pkgs, buildModules, config, lib, ... }:
-#       let
-#         withTH = import ./mingw_w64.nix {
-#           inherit (pkgs) stdenv lib writeScriptBin;
-#           wine = pkgs.buildPackages.winePackages.minimal;
-#           inherit (pkgs.windows) mingw_w64_pthreads;
-#           inherit (pkgs) gmp;
-#           # iserv-proxy needs to come from the buildPackages, as it needs to run on the
-#           # build host.
-#           inherit (config.hsPkgs.buildPackages.iserv-proxy.components.exes) iserv-proxy;
-#           # remote-iserv however needs to come from the regular packages as it has to
-#           # run on the target host.
-#           inherit (packages.remote-iserv.components.exes) remote-iserv;
-#           # we need to use openssl.bin here, because the .dll's are in the .bin expression.
-#           extra-test-libs = [ pkgs.rocksdb pkgs.openssl.bin ];
-
-#         } // { doCrossCheck = true; };
-#        in lib.optionalAttrs pkgs.stdenv.hostPlatform.isWindows  {
-#          packages.cardano-wallet    = withTH;
-#          packages.generics-sop      = withTH;
-#          packages.ether             = withTH;
-#          packages.th-lift-instances = withTH;
-#          packages.aeson             = withTH;
-#          packages.hedgehog          = withTH;
-#          packages.th-orphans        = withTH;
-#          packages.uri-bytestring    = withTH;
-#          packages.these             = withTH;
-#          packages.katip             = withTH;
-#          packages.swagger2          = withTH;
-#          packages.wreq              = withTH;
-#          packages.wai-app-static    = withTH;
-#          packages.log-warper        = withTH;
-#          packages.cardano-sl-util   = withTH;
-#          packages.cardano-sl-crypto = withTH;
-#          packages.cardano-sl-crypto-test = withTH;
-#          packages.cardano-sl-core   = withTH;
-#          packages.cardano-sl        = withTH;
-#          packages.cardano-sl-chain  = withTH;
-#          packages.cardano-sl-db     = withTH;
-#          packages.cardano-sl-networking = withTH;
-#          packages.cardano-sl-infra  = withTH;
-#          packages.cardano-sl-infra-test = withTH;
-#          packages.cardano-sl-client = withTH;
-#          packages.cardano-sl-core-test = withTH;
-#          packages.cardano-sl-chain-test = withTH;
-#          packages.cardano-sl-utxo   = withTH;
-#          packages.cardano-sl-tools    = withTH;
-#          packages.cardano-sl-generator = withTH;
-#          packages.cardano-sl-auxx     = withTH;
-#          packages.cardano-sl-faucet   = withTH;
-#          packages.cardano-sl-binary   = withTH;
-#          packages.cardano-sl-node     = withTH;
-#          packages.cardano-sl-explorer = withTH;
-#          packages.cardano-sl-cluster  = withTH;
-#          packages.cardano-sl-x509     = withTH;
-#          packages.cardano-sl-mnemonic = withTH;
-#          packages.cardano-crypto      = withTH;
-#          packages.math-functions    = withTH;
-#          packages.servant-swagger-ui = withTH;
-#          packages.servant-swagger-ui-redoc = withTH;
-#          packages.trifecta            = withTH;
-#          packages.Chart               = withTH;
-#          packages.active              = withTH;
-#          packages.diagrams            = withTH;
-#          packages.diagrams-lib        = withTH;
-#          packages.diagrams-svg        = withTH;
-#          packages.diagrams-postscript = withTH;
-#          packages.Chart-diagrams      = withTH;
-#       })
-#       # packages we wish to ignore version bounds of.
-#       # this is similar to jailbreakCabal, however it
-#       # does not require any messing with cabal files.
-#       {
-#          packages.katip.components.library.doExactConfig         = true;
-#          packages.serokell-util.components.library.doExactConfig = true;
-#          # trutle wants Win32 < 2.6
-#          packages.turtle.components.library.doExactConfig        = true;
-#       }
-#       ({ pkgs, ... }: {
-#          packages.hfsevents.components.library.frameworks  = [ pkgs.CoreServices ];
-#       })
-#     ];
-#   };
-
   pkgSet = haskell.mkNewPkgSet {
     inherit pkgs;
-    #pkg-def = stackage.${stack-pkgs.resolver};
-    pkg-def = stackage."lts-12.17";
+    pkg-def = stackage.${stack-pkgs.resolver};
     pkg-def-overlays = [
       stack-pkgs.overlay
+      (import ./ghc-custom/default.nix)
+      (hackage: {
+          hsc2hs = hackage.hsc2hs."0.68.4".revisions.default;
+          # stackage 12.17 beautifully omits the Win32 pkg
+          Win32 = hackage.Win32."2.6.2.0".revisions.default;
+      })
     ];
     modules = [
+      {
+         # This needs true, otherwise we miss most of the interesting
+         # modules.
+         packages.ghci.flags.ghci = true;
+         # this needs to be true to expose module
+         #  Message.Remote
+         # as needed by libiserv.
+         packages.libiserv.flags.network = true;
+
+         # enable golden tests on cardano-crypto
+         packages.cardano-crypto.flags.golden-tests = true;
+      }
+
+      ({ config, ... }: {
+          packages.hsc2hs.components.exes.hsc2hs.doExactConfig = true;
+          packages.Win32.components.library.build-tools = [ config.hsPkgs.buildPackages.hsc2hs ];
+          packages.remote-iserv.postInstall = ''
+            cp ${pkgs.windows.mingw_w64_pthreads}/bin/libwinpthread-1.dll $out/bin/
+          '';
+      })
+
+      {
+        packages.conduit.patches            = [ ./patches/conduit-1.3.0.2.patch ];
+        packages.cryptonite-openssl.patches = [ ./patches/cryptonite-openssl-0.7.patch ];
+        packages.streaming-commons.patches  = [ ./patches/streaming-commons-0.2.0.0.patch ];
+        packages.x509-system.patches        = [ ./patches/x509-system-1.6.6.patch ];
+        packages.file-embed-lzma.patches    = [ ./patches/file-embed-lzma-0.patch ];
+        packages.cardano-sl.patches         = [ ./patches/cardano-sl.patch ];
+      }
+
+      # cross compilation logic
+      ({ pkgs, config, lib, ... }:
+      let
+        withTH = import ./mingw_w64.nix {
+          inherit (pkgs') stdenv lib writeScriptBin;
+          wine = pkgs.buildPackages.winePackages.minimal;
+          inherit (pkgs.windows) mingw_w64_pthreads;
+          inherit (pkgs) gmp;
+          # iserv-proxy needs to come from the buildPackages, as it needs to run on the
+          # build host.
+          inherit (config.hsPkgs.buildPackages.iserv-proxy.components.exes) iserv-proxy;
+          # remote-iserv however needs to come from the regular packages as it has to
+          # run on the target host.
+          inherit (packages.remote-iserv.components.exes) remote-iserv;
+          # we need to use openssl.bin here, because the .dll's are in the .bin expression.
+          extra-test-libs = [ pkgs.rocksdb pkgs.openssl.bin ];
+        } // { doCrossCheck = true; };
+       in lib.optionalAttrs pkgs'.stdenv.hostPlatform.isWindows  {
+         packages.cardano-wallet           = withTH;
+         packages.generics-sop             = withTH;
+         packages.ether                    = withTH;
+         packages.th-lift-instances        = withTH;
+         packages.aeson                    = withTH;
+         packages.hedgehog                 = withTH;
+         packages.th-orphans               = withTH;
+         packages.uri-bytestring           = withTH;
+         packages.these                    = withTH;
+         packages.katip                    = withTH;
+         packages.swagger2                 = withTH;
+         packages.wreq                     = withTH;
+         packages.wai-app-static           = withTH;
+         packages.log-warper               = withTH;
+         packages.cardano-sl-util          = withTH;
+         packages.cardano-sl-crypto        = withTH;
+         packages.cardano-sl-crypto-test   = withTH;
+         packages.cardano-sl-core          = withTH;
+         packages.cardano-sl               = withTH;
+         packages.cardano-sl-chain         = withTH;
+         packages.cardano-sl-db            = withTH;
+         packages.cardano-sl-networking    = withTH;
+         packages.cardano-sl-infra         = withTH;
+         packages.cardano-sl-infra-test    = withTH;
+         packages.cardano-sl-client        = withTH;
+         packages.cardano-sl-core-test     = withTH;
+         packages.cardano-sl-chain-test    = withTH;
+         packages.cardano-sl-utxo          = withTH;
+         packages.cardano-sl-tools         = withTH;
+         packages.cardano-sl-generator     = withTH;
+         packages.cardano-sl-auxx          = withTH;
+         packages.cardano-sl-faucet        = withTH;
+         packages.cardano-sl-binary        = withTH;
+         packages.cardano-sl-node          = withTH;
+         packages.cardano-sl-explorer      = withTH;
+         packages.cardano-sl-cluster       = withTH;
+         packages.cardano-sl-x509          = withTH;
+         packages.cardano-sl-mnemonic      = withTH;
+         packages.cardano-crypto           = withTH;
+         packages.math-functions           = withTH;
+         packages.servant-swagger-ui       = withTH;
+         packages.servant-swagger-ui-redoc = withTH;
+         packages.trifecta                 = withTH;
+         packages.Chart                    = withTH;
+         packages.active                   = withTH;
+         packages.diagrams                 = withTH;
+         packages.diagrams-lib             = withTH;
+         packages.diagrams-svg             = withTH;
+         packages.diagrams-postscript      = withTH;
+         packages.Chart-diagrams           = withTH;
+      })
+
+      # Packages we wish to ignore version bounds of.
+      # This is similar to jailbreakCabal, however it
+      # does not require any messing with cabal files.
+      {
+         packages.katip.components.library.doExactConfig         = true;
+         packages.serokell-util.components.library.doExactConfig = true;
+         # turtle wants Win32 < 2.6
+         packages.turtle.components.library.doExactConfig        = true;
+      }
+      ({ pkgs, ... }: {
+         packages.hfsevents.components.library.frameworks  = [ pkgs.CoreServices ];
+      })
     ];
   };
 
