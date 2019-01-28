963a37d435d12466964ba8724aaae371198adec0
Denis Shevchenko <denis.shevchenko@iohk.io>
2019-01-28 15:07:06 +0400

    Update operation, for EOS-wallets.

--- src/Cardano/Wallet/Kernel/DB/HdWallet/Update.hs
+++ src/Cardano/Wallet/Kernel/DB/HdWallet/Update.hs
@@ -3,10 +3,12 @@ module Cardano.Wallet.Kernel.DB.HdWallet.Update (
     updateHdRoot
   , updateHdRootPassword
   , updateHdAccountName
+  , updateHdAccountGap
   ) where
 
 import           Universum
 
+import           Cardano.Wallet.Kernel.AddressPoolGap (AddressPoolGap)
 import           Cardano.Wallet.Kernel.DB.HdRootId (HdRootId)
 import           Cardano.Wallet.Kernel.DB.HdWallet
 import           Cardano.Wallet.Kernel.DB.Util.AcidState
@@ -38,3 +40,20 @@ updateHdAccountName :: HdAccountId
 updateHdAccountName accId name = do
     zoomHdAccountId identity accId $ do
         modifyAndGetNew $ hdAccountName .~ name
+
+updateHdAccountGap :: HdAccountId
+                   -> AddressPoolGap
+                   -> Update' UpdateGapError HdWallets HdAccount
+updateHdAccountGap accId gap =
+    zoomHdAccountId embedErr accId $ do
+        acc <- get
+        case acc ^. hdAccountBase of
+            HdAccountBaseEO _ pKey _ ->
+                modifyAndGetNew $ hdAccountBase .~ HdAccountBaseEO accId pKey gap
+            HdAccountBaseFO _ ->
+                -- It's an error: we try to update a gap in account with FO-branch.
+                throwError $ UpdateGapErrorFOAccount accId
+  where
+    embedErr :: UnknownHdAccount -> UpdateGapError
+    embedErr (UnknownHdAccountRoot rootId) = UpdateGapErrorUnknownHdAccountRoot rootId
+    embedErr (UnknownHdAccount accountId)  = UpdateGapErrorUnknownHdAccount accountId
