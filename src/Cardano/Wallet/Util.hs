0165609e2a9d3d650104bde094e1a247d68e13f5
KtorZ <matthias.benkort@gmail.com>
2019-02-06 12:26:27 +0100

    introduce utility functions to write more useful buildable instances

--- src/Cardano/Wallet/Util.hs
+++ src/Cardano/Wallet/Util.hs
@@ -1,9 +1,25 @@
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+-- ^ Disabling the above because we use 'trace' in the 'traceBuildable'
+-- function. Note that, there's now an extra rule in our hlint rules that
+-- make sure there's no 'traceBuildable' hanging in the code, such that it's
+-- really used for debugging only.
+
 -- | Module for small utility functions.
 module Cardano.Wallet.Util
        ( -- * String manipulation
          headToLower
        , stripFieldPrefix
        , mkJsonKey
+
+       -- * Debug
+       , traceBuildable
+
+       -- * Formatting
+       , buildIndent
+       , buildList
+       , buildMap
+       , buildTrunc
+       , buildTuple2
 
        -- * Miscellaneous
        , eitherToParser
@@ -15,14 +31,86 @@ module Cardano.Wallet.Util
        , showApiUtcTime
        ) where
 
-import           Universum
+import           Universum hiding (truncate)
 
 import           Data.Aeson.Types (Parser)
 import           Data.Char (isUpper, toLower)
+import qualified Data.Foldable as F
+import qualified Data.Map.Strict as Map
+import           Data.Text.Internal.Builder (Builder)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Builder as B
 import qualified Data.Time as Time
-import           Formatting (bprint, build, formatToString, (%))
+import           Formatting (Format, bprint, build, formatToString, later, (%))
 import qualified Formatting.Buildable
 import qualified Prelude
+
+
+-- * Debug
+
+traceBuildable :: Buildable a => a -> b -> b
+traceBuildable a b =
+    trace (formatToString build $ a) b
+
+
+-- * Formatting
+
+buildIndent
+    :: Int
+    -> Format Builder (a -> Builder)
+    -> Format r (a -> r)
+buildIndent n buildA =
+    later $ fmapBuilder (safeInit . TL.unlines . fmap (prefix <>) . TL.lines) . bprint buildA
+  where
+    prefix = TL.replicate (fromIntegral n) " "
+    safeInit t
+        | TL.null t = t
+        | otherwise = TL.init t
+
+buildTrunc
+    :: Format Builder (a -> Builder)
+    -> Format r (a -> r)
+buildTrunc buildA = later $ fmapBuilder (truncate' 8) . bprint buildA
+  where
+    truncate' n txt
+        | TL.length txt <= 2*n + 3 = txt
+        | otherwise = TL.take n txt <> "..." <> TL.takeEnd n txt
+
+buildMap
+    :: Format Builder (a -> Builder)
+    -> String
+    -> Format Builder (b -> Builder)
+    -> Format r (Map a b -> r)
+buildMap buildA sep buildB =
+    later $ bprint (buildList $ buildTuple2 buildA sep buildB) . Map.toList
+
+buildList
+    :: Foldable t
+    => Format Builder (a -> Builder)
+    -> Format r (t a -> r)
+buildList buildA =
+    later $ buildList' . F.toList
+  where
+    buildLine = ("- " <>) . fmapBuilder (TL.drop 2) . bprint (buildIndent 2 buildA)
+    buildList' []    = "[]"
+    buildList' [h]   = buildLine h
+    buildList' (h:q) = buildLine h <> "\n" <> buildList' q
+
+buildTuple2
+    :: Format Builder (a -> Builder)
+    -> String
+    -> Format Builder (b -> Builder)
+    -> Format r ((a, b) -> r)
+buildTuple2 buildA sep buildB =
+    later $ \(a, b) -> (bprint buildA a) <> " " <> fromString sep <> " " <> (bprint buildB b)
+
+fmapBuilder
+    :: (TL.Text -> TL.Text)
+    -> Builder
+    -> Builder
+fmapBuilder fn =
+    B.fromLazyText . fn . B.toLazyText
+
 
 -- * Miscellaneous
 
